39f7757c9a565472d93351ba0d855526200902cf
guangtao.wang <guangtao.wang@jd.com>
2021-02-19 13:24:13 -0800

    pytorch lightning

--- model_envs.py
+++ model_envs.py
@@ -1,9 +1,9 @@
-import envs
-
+from transformers import T5ForConditionalGeneration, AutoTokenizer, T5Config, T5Tokenizer
 from hgntransformers import (BertConfig, BertTokenizer, BertModel,
                              RobertaConfig, RobertaTokenizer, RobertaModel,
                              AlbertConfig, AlbertTokenizer, AlbertModel)
 from hgntransformers import (BertModel, XLNetModel, RobertaModel)
+
 
 ############################################################
 # Model Related Global Varialbes
@@ -15,4 +15,5 @@ MODEL_CLASSES = {
     'bert': (BertConfig, BertModel, BertTokenizer),
     'roberta': (RobertaConfig, RobertaModel, RobertaTokenizer),
     'albert': (AlbertConfig, AlbertModel, AlbertTokenizer),
+    'unifiedqa': (T5Config, T5ForConditionalGeneration, AutoTokenizer)
 }
