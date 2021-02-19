8e417385f4ddb011a409aa8a3f328495630e52bf
guangtao.wang <guangtao.wang@jd.com>
2021-02-19 12:08:16 -0800

    pytorch lightning

--- dataExampleloader.py
+++ dataExampleloader.py
@@ -1,8 +1,9 @@
 import sys
 
 from plmodels.jd_argument_parser import default_train_parser, complete_default_train_parser, json_to_argv
-from plmodels.pldata_processing import Example, InputFeatures, DataHelper
+from plmodels.pldata_processing import Example, InputFeatures, DataHelper, HotpotDataset
 import logging
+from torch.utils.data import DataLoader
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
@@ -40,6 +41,18 @@ helper = DataHelper(gz=True, config=args)
 
 # Set datasets
 # train_dataloader = helper.train_loader
-# dev_example_dict = helper.dev_example_dict
-# dev_feature_dict = helper.dev_feature_dict
+dev_example_dict = helper.dev_example_dict
+dev_feature_dict = helper.dev_feature_dict
 dev_dataloader = helper.dev_loader
+
+def get_data_loader(dataset):
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=4,
+        shuffle=False,
+        num_workers=max(1, 6),
+        collate_fn=HotpotDataset.collate_fn
+    )
+    return dataloader
+
+dev_loader = get_data_loader(dataset=dev_dataloader)
