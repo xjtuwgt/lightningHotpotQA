fa27c4fb83cbeaeddaf864ddd6c6bc5284d83223
guangtao.wang <guangtao.wang@jd.com>
2021-02-18 17:44:49 -0800

    pytorch lightning

--- envs.py
+++ envs.py
@@ -9,7 +9,8 @@ PROJECT_FOLDER = os.path.dirname(__file__)
 sys.path.append(join(PROJECT_FOLDER, 'hgntransformers'))
 
 # Define the dataset folder and model folder based on environment
-HOME_DATA_FOLDER = '/ssd/HGN/data'
+# HOME_DATA_FOLDER = '/ssd/HGN/data'
+HOME_DATA_FOLDER = join(PROJECT_FOLDER, 'data')
 DATASET_FOLDER = join(HOME_DATA_FOLDER, 'dataset')
 MODEL_FOLDER = join(HOME_DATA_FOLDER, 'models')
 KNOWLEDGE_FOLDER = join(HOME_DATA_FOLDER, 'knowledge')
