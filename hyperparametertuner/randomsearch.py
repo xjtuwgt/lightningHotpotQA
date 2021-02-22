502d951004aff7ef1e7368b9dab268f5876dfac6
guangtao.wang <guangtao.wang@jd.com>
2021-02-21 19:36:58 -0800

    pytorch lightning

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -46,7 +46,7 @@ def HypeParameterSpace():
     learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [2e-5, 3e-5, 4e-5]}
     per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2, 4]}
     gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [2, 4]}
-    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [15, 20]}
+    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [5, 10, 15]}
     frozen_layer_num = {'name': 'frozen_layer_number', 'type': 'choice', 'values': [0]}
     gnn_drop = {'name': 'gnn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
     bi_attn_drop = {'name': 'bi_attn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
