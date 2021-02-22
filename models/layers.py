f81ab014e6aff841d836c11fbacecf7f75ff3218
guangtao.wang <guangtao.wang@jd.com>
2021-02-21 18:20:28 -0800

    pytorch lightning

--- models/layers.py
+++ models/layers.py
@@ -416,9 +416,12 @@ class LSTMWrapper(nn.Module):
         output = input
         outputs = []
 
+        print(input_lengths)
+
         if input_lengths is not None:
             lens = input_lengths.data.cpu().numpy()
 
+            print(lens)
         for i in range(self.n_layer):
             output = F.dropout(output, p=self.dropout, training=self.training)
 
