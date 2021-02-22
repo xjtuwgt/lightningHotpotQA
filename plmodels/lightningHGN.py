5200714321010ab21e166c09c56f88c4e0e265e6
guangtao.wang <guangtao.wang@jd.com>
2021-02-22 08:28:46 -0800

    pytorch lightning

--- plmodels/lightningHGN.py
+++ plmodels/lightningHGN.py
@@ -4,9 +4,8 @@ from os.path import join
 import numpy as np
 import json
 from torch.utils.data import DataLoader
-# from transformers import AdamW, get_linear_schedule_with_warmup
-from hgntransformers import AdamW, get_linear_schedule_with_warmup
-# from transformers import T5ForConditionalGeneration, AutoTokenizer, T5Config
+from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers import T5ForConditionalGeneration, AutoTokenizer, T5Config
 from hgntransformers import (BertConfig, BertTokenizer, BertModel,
                              RobertaConfig, RobertaTokenizer, RobertaModel,
                              AlbertConfig, AlbertTokenizer, AlbertModel)
@@ -27,7 +26,7 @@ MODEL_CLASSES = {
     'bert': (BertConfig, BertModel, BertTokenizer),
     'roberta': (RobertaConfig, RobertaModel, RobertaTokenizer),
     'albert': (AlbertConfig, AlbertModel, AlbertTokenizer),
-    # 'unifiedqa': (T5Config, T5ForConditionalGeneration, AutoTokenizer)
+    'unifiedqa': (T5Config, T5ForConditionalGeneration, AutoTokenizer)
 }
 
 class lightningHGN(pl.LightningModule):
