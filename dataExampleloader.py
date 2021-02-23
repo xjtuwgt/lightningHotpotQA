e276bb24b893d2d8da527d4a03ac2f2d54eb7ed4
guangtao.wang <guangtao.wang@jd.com>
2021-02-23 10:34:03 -0800

    pytorch lightning

--- dataExampleloader.py
+++ dataExampleloader.py
@@ -38,7 +38,7 @@ for a in args_dict:
 #########################################################################
 # Read Data
 ##########################################################################
-helper = DataHelper(gz=True, config=args)
+helper = DataHelper(gz=True, config=args, f_type='long')
 
 # Set datasets
 # train_dataloader = helper.train_loader
