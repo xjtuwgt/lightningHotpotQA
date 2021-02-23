dc5cf165ac324bb2c0329a4fb9d7f749ba637856
guangtao.wang <guangtao.wang@jd.com>
2021-02-23 10:31:42 -0800

    pytorch lightning

--- plmodels/pldata_processing.py
+++ plmodels/pldata_processing.py
@@ -131,7 +131,7 @@ class InputFeatures(object):
         self.end_position = end_position
 
 class DataHelper:
-    def __init__(self, gz=True, config=None, reverse=False, ranker=None):
+    def __init__(self, gz=True, config=None, f_type=None):
         self.Dataset = HotpotDataset
         self.gz = gz
         self.suffix = '.pkl.gz' if gz else '.pkl'
@@ -152,38 +152,27 @@ class DataHelper:
 
         self.config = config
 
-        self.reverse = reverse
-        self.ranker = ranker
+        self.f_type = f_type
 
     def get_feature_file(self, tag):
-        if self.ranker is None:
+        if self.f_type is None:
             cached_filename = get_cached_filename('features', self.config)
         else:
-            if self.reverse:
-                cached_filename = get_cached_filename('{}_reverse_features'.format(self.ranker), self.config)
-            else:
-                cached_filename = get_cached_filename('{}_features'.format(self.ranker), self.config)
+            cached_filename = get_cached_filename('{}_features'.format(self.f_type), self.config)
         return join(self.data_dir, tag, cached_filename)
 
     def get_example_file(self, tag):
-        if self.ranker is None:
+        if self.f_type is None:
             cached_filename = get_cached_filename('examples', self.config)
         else:
-            if self.reverse:
-                cached_filename = get_cached_filename('{}_reverse_examples'.format(self.ranker), self.config)
-            else:
-                cached_filename = get_cached_filename('{}_examples'.format(self.ranker), self.config)
-
+            cached_filename = get_cached_filename('{}_examples'.format(self.f_type), self.config)
         return join(self.data_dir, tag, cached_filename)
 
     def get_graph_file(self, tag):
-        if self.ranker is None:
+        if self.f_type is None:
             cached_filename = get_cached_filename('graphs', self.config)
         else:
-            if self.reverse:
-                cached_filename = get_cached_filename('{}_reverse_graphs'.format(self.ranker), self.config)
-            else:
-                cached_filename = get_cached_filename('{}_graphs'.format(self.ranker), self.config)
+            cached_filename = get_cached_filename('{}_graphs'.format(self.f_type), self.config)
         return join(self.data_dir, tag, cached_filename)
 
     @property
