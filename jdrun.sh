e16845c8b1d26547bff954650d241350dbe06f77
guangtao.wang <guangtao.wang@jd.com>
2021-02-22 19:47:11 -0800

    pytorch lightning

--- jdrun.sh
+++ jdrun.sh
@@ -58,12 +58,12 @@ preprocess() {
 #        # Output: ner.json
 #        python scripts/2_extract_ner.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json
 #
-        echo "3. Paragraph ranking"
-        # Output: para_ranking.json
-        python scripts/3_prepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/hotpot_ss_$DATA_TYPE.csv
-
-        # switch to RoBERTa for final leaderboard
-        python scripts/3_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/pytorch_model.bin --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/hotpot_ss_$DATA_TYPE.csv --model_name_or_path roberta-large --model_type roberta --max_seq_length 256 --per_gpu_eval_batch_size 128 --fp16
+#        echo "3. Paragraph ranking"
+#        # Output: para_ranking.json
+#        python scripts/3_prepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/hotpot_ss_$DATA_TYPE.csv
+#
+#        # switch to RoBERTa for final leaderboard
+#        python scripts/3_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/pytorch_model.bin --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/hotpot_ss_$DATA_TYPE.csv --model_name_or_path roberta-large --model_type roberta --max_seq_length 256 --per_gpu_eval_batch_size 128 --fp16
 
         echo "4. MultiHop Paragraph Selection"
         # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, para_ranking.json
