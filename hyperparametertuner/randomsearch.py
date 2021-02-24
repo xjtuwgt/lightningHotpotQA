fb4b83ecaa442810bb4368b4f6793f4eb6fc8e68
guangtao.wang <guangtao.wang@jd.com>
2021-02-23 19:42:46 -0800

    pytorch lightning

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -43,13 +43,13 @@ def rand_search_parameter(space: dict):
         raise ValueError('Training batch mode %s not supported' % para_type)
 
 def HypeParameterSpace():
-    learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [1e-5, 3e-5]}
+    learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [1e-5, 2e-5]}
     per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2, 4]}
     gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [1, 2]}
     sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [5, 10, 15]}
     frozen_layer_num = {'name': 'frozen_layer_number', 'type': 'choice', 'values': [0]}
-    gnn_drop = {'name': 'gnn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
-    bi_attn_drop = {'name': 'bi_attn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
+    gnn_drop = {'name': 'gnn_drop', 'type': 'choice', 'values': [0.3]}
+    bi_attn_drop = {'name': 'bi_attn_drop', 'type': 'choice', 'values': [0.3]}
     trans_drop = {'name': 'trans_drop', 'type': 'choice', 'values': [0.3]}
     lstm_drop = {'name': 'lstm_drop', 'type': 'choice', 'values': [0.3]}
     num_train_epochs = {'name': 'num_train_epochs', 'type': 'choice', 'values': [10]}
