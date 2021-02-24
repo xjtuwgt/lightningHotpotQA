821ee914aa03b8ff3f640afb9a054985364ba8ba
guangtao.wang <guangtao.wang@jd.com>
2021-02-23 19:32:48 -0800

    pytorch lightning

--- plmodels/lightningHGN.py
+++ plmodels/lightningHGN.py
@@ -218,8 +218,7 @@ class lightningHGN(pl.LightningModule):
         json.dump(best_metrics, open(output_eval_file, 'w'))
         #############################################################################
         # self.log('valid_loss', avg_loss, 'joint_f1', best_metrics['joint_f1'], on_epoch=True, prog_bar=True, sync_dist=True)
-        avg_loss = best_metrics['joint_f1']
-        joint_f1 = avg_loss
+        joint_f1 = torch.Tensor([best_metrics['joint_f1']])[0].to(avg_loss.device)
         self.log('joint_f1', joint_f1, on_epoch=True, prog_bar=True, sync_dist=True)
         #############################################################################
         return best_metrics, best_threshold
