f700ab85c2269c29e3b3bda622e72d15b0bcb157
guangtao.wang <guangtao.wang@jd.com>
2021-02-26 08:55:36 -0800

    hotpotqa

--- download_model.sh
+++ download_model.sh
@@ -10,12 +10,12 @@ export PYTHONPATH=$PYTHONPATH:$DIR_TMP:$DIR_TMP/hgntransformers
 export PYTORCH_PRETRAINED_BERT_CACHE=$DATA_ROOT/models/pretrained_cache
 
 
-# 0. Download Roberta pretrained model
+# 0. Download Roberta pretrained model: ahotrod/roberta_large_squad2
 ROBERTA_SQUAD2=https://huggingface.co/ahotrod/roberta_large_squad2/resolve/main/
 roberta() {
-    [[ -d $DATA_ROOT ]] || mkdir -p $DATA_ROOT/models/pretrained/roberta-large
+    [[ -d $DATA_ROOT ]] || mkdir -p $DATA_ROOT/models/pretrained/roberta-large/ahotrod
 
-    wget -P $DATA_ROOT/models/pretrained/roberta-large/ $ROBERTA_SQUAD2/config.json
+    wget -P $DATA_ROOT/models/pretrained/roberta-large/ahotrod $ROBERTA_SQUAD2/config.json
 }
 
 for proc in "roberta"
