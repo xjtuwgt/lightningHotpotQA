bd0681c8930d4625b2e340d5f2312c2ff718db5a
guangtao.wang <guangtao.wang@jd.com>
2021-02-26 08:40:01 -0800

    hotpotqa

---
+++ download_model.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# DEFINE data related (please make changes according to your configurations)
+# DATA ROOT folder where you put data files
+DATA_ROOT=./data/
+
+# Add current pwd to PYTHONPATH
+export DIR_TMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH=$PYTHONPATH:$DIR_TMP:$DIR_TMP/hgntransformers
+export PYTORCH_PRETRAINED_BERT_CACHE=$DATA_ROOT/models/pretrained_cache
+
+
+# 0. Download Roberta pretrained model
+ROBERTA_SQUAD2=https://huggingface.co/ahotrod/roberta_large_squad2/blob/main
+download_roberta() {
+    [[ -d $DATA_ROOT ]] || mkdir -p $DATA_ROOT/models/pretrained/roberta-large
+
+    wget -P $DATA_ROOT/models/pretrained/roberta-large/ $ROBERTA_SQUAD2/config.json
+}
+
+for proc in "download_roberta"
+do
+    $proc
+done
