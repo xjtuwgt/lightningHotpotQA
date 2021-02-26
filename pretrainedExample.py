fd6b91f6f3cec6f8421b928b3bb93fb8ab7efafe
guangtao.wang <guangtao.wang@jd.com>
2021-02-26 09:45:04 -0800

    hotpotqa

--- pretrainedExample.py
+++ pretrainedExample.py
@@ -1,4 +1,7 @@
 from jdscripts.pretrainedencoders import load_model
 
 
-load_model(model_type='roberta')
+model = load_model(model_type='roberta')
+
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
