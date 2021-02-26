c54293b6f0b917acc74e474b852a960afb379ae7
guangtao.wang <guangtao.wang@jd.com>
2021-02-26 14:08:00 -0800

    hotpotqa

--- pretrainedExample.py
+++ pretrainedExample.py
@@ -3,6 +3,8 @@ from jdscripts.pretrainedencoders import load_model, save_model_as_pkl
 roberta_model_name = 'ahotrod/roberta_large_squad2'
 albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
 model = load_model(model_type='roberta', model_name=roberta_model_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
 save_model_as_pkl(encoder=model, model_name=albert_model_name)
 
 # model = load_model(model_type='albert', model_name=albert_model_name)
