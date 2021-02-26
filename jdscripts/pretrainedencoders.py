ca04f53cc6f24023c4d1364ec8ef4abf472c7ef5
guangtao.wang <guangtao.wang@jd.com>
2021-02-26 14:32:19 -0800

    hotpotqa

--- jdscripts/pretrainedencoders.py
+++ jdscripts/pretrainedencoders.py
@@ -1,7 +1,7 @@
 ###Roberta-large on squad2 --> ahotrod/roberta_large_squad2
 ###AL-BERT-large on squad2
 from os.path import join
-from envs import PRETRAINED_MODEL_FOLDER
+from envs import PRETRAINED_MODEL_FOLDER, OUTPUT_FOLDER
 from model_envs import MODEL_CLASSES
 import torch
 
@@ -15,3 +15,10 @@ def save_model_as_pkl(encoder, model_name):
     pickle_model_name = join(PRETRAINED_MODEL_FOLDER, model_name, f'encoder.pkl')
     torch.save({k: v.cpu() for k, v in encoder.state_dict().items()}, pickle_model_name)
     print('Saved pickle name = {}'.format(pickle_model_name))
+
+def load_hotpotqa_model(model_type, model_name):
+    config_class, model_class, tokenizer_class = MODEL_CLASSES[model_type]
+    model = model_class.from_pretrained(model_name)
+    encoder_pickle_name = join(OUTPUT_FOLDER, model_type, 'encoder.pkl')
+    model.load_state_dict(torch.load(encoder_pickle_name))
+    return model
--- pretrainedExample.py
+++ pretrainedExample.py
@@ -1,18 +1,20 @@
-from jdscripts.pretrainedencoders import load_model, save_model_as_pkl
+from jdscripts.pretrainedencoders import load_model, save_model_as_pkl, load_hotpotqa_model
 
-roberta_model_name = 'ahotrod/roberta_large_squad2'
-albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
-model = load_model(model_type='roberta', model_name=roberta_model_name)
-for name, param in model.named_parameters():
-    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-save_model_as_pkl(encoder=model, model_name=roberta_model_name)
-
-model = load_model(model_type='albert', model_name=albert_model_name)
-for name, param in model.named_parameters():
-    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-save_model_as_pkl(encoder=model, model_name=albert_model_name)
+# roberta_model_name = 'ahotrod/roberta_large_squad2'
+# albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
+# model = load_model(model_type='roberta', model_name=roberta_model_name)
+# for name, param in model.named_parameters():
+#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+# save_model_as_pkl(encoder=model, model_name=roberta_model_name)
+#
+# model = load_model(model_type='albert', model_name=albert_model_name)
+# for name, param in model.named_parameters():
+#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+# save_model_as_pkl(encoder=model, model_name=albert_model_name)
 
 # model = load_model(model_type='albert', model_name=albert_model_name)
 #
 # for name, param in model.named_parameters():
 #     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+
+model = load_hotpotqa_model(model_type='roberta', model_name='roberta-large')
