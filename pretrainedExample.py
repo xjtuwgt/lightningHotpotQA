1fa081c7af4410544a4a2fca6fb2b18511f24864
guangtao.wang <guangtao.wang@jd.com>
2021-02-26 09:57:36 -0800

    hotpotqa

--- pretrainedExample.py
+++ pretrainedExample.py
@@ -5,3 +5,4 @@ model = load_model(model_type='roberta')
 
 for name, param in model.named_parameters():
     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+
