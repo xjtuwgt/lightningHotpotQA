de60ca2928da5b72393219465be7a4dd9537ba97
guangtao.wang <guangtao.wang@jd.com>
2021-02-27 10:00:31 -0800

    hotpotqa

--- pretrainedExample.py
+++ pretrainedExample.py
@@ -1,4 +1,4 @@
-from jdscripts.pretrainedencoders import load_model, save_model_as_pkl, load_hotpotqa_model
+from jdscripts.pretrainedencoders import load_model, save_model_as_pkl, load_hotpotqa_model, save_hotpotqa_model
 
 # roberta_model_name = 'ahotrod/roberta_large_squad2'
 # albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
@@ -16,7 +16,10 @@ from jdscripts.pretrainedencoders import load_model, save_model_as_pkl, load_hot
 #
 # for name, param in model.named_parameters():
 #     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-
-model = load_hotpotqa_model(model_type='roberta', model_name='roberta-large')
+model_type = 'roberta'
+model_name = 'roberta-large'
+model = load_hotpotqa_model(model_type=model_type, model_name=model_name)
 for name, param in model.named_parameters():
     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+
+save_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
