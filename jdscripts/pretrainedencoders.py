0e5cfd34c5a82fefd1ab7b1399e53b87a7338278
guangtao.wang <guangtao.wang@jd.com>
2021-02-26 21:35:23 -0800

    hotpotqa

--- jdscripts/pretrainedencoders.py
+++ jdscripts/pretrainedencoders.py
@@ -4,6 +4,7 @@ from os.path import join
 from envs import PRETRAINED_MODEL_FOLDER, OUTPUT_FOLDER
 from model_envs import MODEL_CLASSES
 import torch
+import os
 
 def load_model(model_type, model_name):
     config_class, model_class, tokenizer_class = MODEL_CLASSES[model_type]
@@ -24,4 +25,12 @@ def load_hotpotqa_model(model_type, model_name):
     return model
 
 def save_hotpotqa_model(encoder, model_type, model_name):
-    join(PRETRAINED_MODEL_FOLDER, )
+    model_type_folder = join(PRETRAINED_MODEL_FOLDER, model_type)
+    if not os.path.exists(model_type_folder):
+        os.makedirs(model_type_folder)
+    model_type_name_folder = join(model_type_folder, model_name + '_hotpotqa')
+    if not os.path.exists(model_type_name_folder):
+        os.makedirs(model_type_name_folder)
+    pickle_model_name = join(model_type_name_folder, f'encoder.pkl')
+    torch.save({k: v.cpu() for k, v in encoder.state_dict().items()}, pickle_model_name)
+    print('Saved pickle name = {}'.format(pickle_model_name))
