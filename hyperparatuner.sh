de07d19b372c9379ede279a9c996589d1cc0760c
guangtao.wang <guangtao.wang@jd.com>
2021-02-28 11:37:16 -0800

    hotpotqa

--- hyperparatuner.sh
+++ hyperparatuner.sh
@@ -6,13 +6,12 @@ PYTHON_VIRTUAL_ENVIRONMENT=hotpotqa
 source ${CONDA_ROOT}/etc/profile.d/conda.sh
 conda activate $PYTHON_VIRTUAL_ENVIRONMENT
 
-python pretrainedExample.py
-#JOBS_PATH=jdhgn_jobs
-#LOGS_PATH=jdhgn_logs
-#for ENTRY in "${JOBS_PATH}"/*.sh; do
-#  chmod +x $ENTRY
-#  FILE_NAME="$(basename "$ENTRY")"
-#  echo $FILE_NAME
-#  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=4 $LOGS_PATH/$FILE_NAME.log $ENTRY &
-#  sleep 20
-#done
+JOBS_PATH=jdhgn_jobs
+LOGS_PATH=jdhgn_logs
+for ENTRY in "${JOBS_PATH}"/*.sh; do
+  chmod +x $ENTRY
+  FILE_NAME="$(basename "$ENTRY")"
+  echo $FILE_NAME
+  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=4 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  sleep 20
+done
