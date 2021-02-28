c7a4349fab118644c15ad756641becdc0b37be45
guangtao.wang <guangtao.wang@jd.com>
2021-02-27 19:00:12 -0800

    hotpotqa

--- pretrainedExample.py
+++ pretrainedExample.py
@@ -1,16 +1,16 @@
 from jdscripts.pretrainedencoders import load_model, save_model_as_pkl, load_hotpotqa_model, save_hotpotqa_model
 
-# roberta_model_name = 'ahotrod/roberta_large_squad2'
-# albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
-# model = load_model(model_type='roberta', model_name=roberta_model_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# save_model_as_pkl(encoder=model, model_name=roberta_model_name)
-#
-# model = load_model(model_type='albert', model_name=albert_model_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# save_model_as_pkl(encoder=model, model_name=albert_model_name)
+roberta_model_name = 'ahotrod/roberta_large_squad2'
+albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
+model = load_model(model_type='roberta', model_name=roberta_model_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+save_model_as_pkl(encoder=model, model_name=roberta_model_name)
+
+model = load_model(model_type='albert', model_name=albert_model_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+save_model_as_pkl(encoder=model, model_name=albert_model_name)
 
 # model = load_model(model_type='albert', model_name=albert_model_name)
 #
@@ -21,5 +21,4 @@ model_name = 'roberta-large'
 model = load_hotpotqa_model(model_type=model_type, model_name=model_name)
 for name, param in model.named_parameters():
     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-
 save_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
