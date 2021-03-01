b851143c1fcda581516d84bb78c4726cf8a93e3d
guangtao.wang <guangtao.wang@jd.com>
2021-03-01 12:08:32 -0800

    lowercase

--- pretrainedExample.py
+++ pretrainedExample.py
@@ -1,31 +1,26 @@
 from jdscripts.pretrainedencoders import load_model, save_model_as_pkl, load_hotpotqa_model, save_hotpotqa_model
 from jdscripts.pretrainedencoders import load_model_with_enconder
 
+roberta_model_name = 'ahotrod/roberta_large_squad2'
+albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
+model = load_model(model_type='roberta', model_name=roberta_model_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+save_model_as_pkl(encoder=model, model_name=roberta_model_name)
 
-# roberta_model_name = 'ahotrod/roberta_large_squad2'
-# albert_model_name = 'mfeb/albert-xxlarge-v2-squad2'
-# model = load_model(model_type='roberta', model_name=roberta_model_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# save_model_as_pkl(encoder=model, model_name=roberta_model_name)
-#
-# model = load_model(model_type='albert', model_name=albert_model_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# save_model_as_pkl(encoder=model, model_name=albert_model_name)
-#
-# # model = load_model(model_type='albert', model_name=albert_model_name)
-# #
-# # for name, param in model.named_parameters():
-# #     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# model_type = 'roberta'
-# model_name = 'roberta-large'
-# model = load_hotpotqa_model(model_type=model_type, model_name=model_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# save_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
-#
-# model_type = 'roberta'
-# model_name = 'roberta-large'
-# encoder_name = 'roberta/roberta-large_hotpotqa'
-# model = load_model_with_enconder(model_type=model_type, model_name=model_name, encoder_model_name=encoder_name)
+model = load_model(model_type='albert', model_name=albert_model_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+save_model_as_pkl(encoder=model, model_name=albert_model_name)
+
+model_type = 'roberta'
+model_name = 'roberta-large'
+model = load_hotpotqa_model(model_type=model_type, model_name=model_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+save_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
+
+model_type = 'roberta'
+model_name = 'roberta-large'
+encoder_name = 'roberta/roberta-large_hotpotqa'
+model = load_model_with_enconder(model_type=model_type, model_name=model_name, encoder_model_name=encoder_name)
