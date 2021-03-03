0ed3f99c4a3c3a26d28421043ce4b35ef9358888
guangtao.wang <guangtao.wang@jd.com>
2021-03-02 21:19:48 -0800

    lowercase

--- hyperparatuner.sh
+++ hyperparatuner.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-HOME=/mnt/cephfs2/nlp/guangtao.wang
-CONDA_ROOT=${HOME}/anaconda3
-PYTHON_VIRTUAL_ENVIRONMENT=hotpotqa
-source ${CONDA_ROOT}/etc/profile.d/conda.sh
-conda activate $PYTHON_VIRTUAL_ENVIRONMENT
+#HOME=/mnt/cephfs2/nlp/guangtao.wang
+#CONDA_ROOT=${HOME}/anaconda3
+#PYTHON_VIRTUAL_ENVIRONMENT=hotpotqa
+#source ${CONDA_ROOT}/etc/profile.d/conda.sh
+#conda activate $PYTHON_VIRTUAL_ENVIRONMENT
+
+eval "$(conda shell.bash hook)"
+conda activate hotpotqa
 
 JOBS_PATH=jdhgn_jobs
 LOGS_PATH=jdhgn_logs
