48f1acfbb23db01c64ea938f259cc040471dd3f3
guangtao.wang <guangtao.wang@jd.com>
2021-03-02 21:32:35 -0800

    lowercase

--- hyperparatuner.sh
+++ hyperparatuner.sh
@@ -8,12 +8,14 @@
 eval "$(conda shell.bash hook)"
 conda activate hotpotqa
 
-JOBS_PATH=jdhgn_jobs
-LOGS_PATH=jdhgn_logs
-for ENTRY in "${JOBS_PATH}"/*.sh; do
-  chmod +x $ENTRY
-  FILE_NAME="$(basename "$ENTRY")"
-  echo $FILE_NAME
-  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=4 $LOGS_PATH/$FILE_NAME.log $ENTRY &
-  sleep 20
-done
+
+
+#JOBS_PATH=jdhgn_jobs
+#LOGS_PATH=jdhgn_logs
+#for ENTRY in "${JOBS_PATH}"/*.sh; do
+#  chmod +x $ENTRY
+#  FILE_NAME="$(basename "$ENTRY")"
+#  echo $FILE_NAME
+#  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=4 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+#  sleep 20
+#done
