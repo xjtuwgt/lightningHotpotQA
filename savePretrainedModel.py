0107bd426951632f67df9a062b5541e0882f4c39
guangtao.wang <guangtao.wang@jd.com>
2021-03-04 09:31:14 -0800

    save model

--- savePretrainedModel.py
+++ savePretrainedModel.py
@@ -34,14 +34,14 @@ from jdscripts.pretrainedencoders import model_intialization_test
 # encoder_name = 'roberta/roberta-large_hotpotqa'
 # # model = load_model_with_enconder(model_type=model_type, model_name=model_name, encoder_model_name=encoder_name)
 
-# model_type = 'roberta'
-# model_name = 'roberta-large'
-# exp_name = 'train.roberta.bs2lr1e-05.seed1000'
-# encoder_pickle_name = 'encoder_1.pkl'
-# model = load_hgn_hotpotqa_model(model_type=model_type, model_name=model_name, exp_name=exp_name, encoder_pkl_name=encoder_pickle_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# save_hgn_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
+model_type = 'roberta'
+model_name = 'roberta-large'
+exp_name = 'train.roberta.bs2lr1e-05.seed1000'
+encoder_pickle_name = 'encoder_1.pkl'
+model = load_hgn_hotpotqa_model(model_type=model_type, model_name=model_name, exp_name=exp_name, encoder_pkl_name=encoder_pickle_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+save_hgn_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
 
 model_type = 'roberta'
 model_name = 'roberta-large'
