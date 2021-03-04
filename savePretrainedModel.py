10f77df6149f74fab99f8dbc9523f3a551c7209b
guangtao.wang <guangtao.wang@jd.com>
2021-03-04 09:20:21 -0800

    save model

--- savePretrainedModel.py
+++ savePretrainedModel.py
@@ -39,13 +39,13 @@ model_name = 'roberta-large'
 exp_name = 'train.roberta.bs2lr1e-05.seed1000'
 encoder_pickle_name = 'encoder_1.pkl'
 model = load_hgn_hotpotqa_model(model_type=model_type, model_name=model_name, exp_name=exp_name, encoder_pkl_name=encoder_pickle_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
-# save_hgn_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
-#
-# model_type = 'roberta'
-# model_name = 'roberta-large'
-# prtrained_name = 'roberta-large_hgn'
-# model = model_intialization_test(model_type=model_type, model_name=model_name, petrained_name=prtrained_name)
-# for name, param in model.named_parameters():
-#     print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
+save_hgn_hotpotqa_model(encoder=model, model_type=model_type, model_name=model_name)
+
+model_type = 'roberta'
+model_name = 'roberta-large'
+prtrained_name = 'roberta-large_hgn'
+model = model_intialization_test(model_type=model_type, model_name=model_name, petrained_name=prtrained_name)
+for name, param in model.named_parameters():
+    print('Parameter {}: {}, require_grad = {}'.format(name, str(param.size()), str(param.requires_grad)))
