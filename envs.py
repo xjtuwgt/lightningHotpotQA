fd2d0190bbb4055f8a667ccfd00af6384d7d61c8
guangtao.wang <guangtao.wang@jd.com>
2021-03-04 08:33:57 -0800

    save model

--- envs.py
+++ envs.py
@@ -12,10 +12,15 @@ sys.path.append(join(PROJECT_FOLDER))
 # Define the dataset folder and model folder based on environment
 # HOME_DATA_FOLDER = '/ssd/HGN/data'
 HOME_DATA_FOLDER = join(PROJECT_FOLDER, 'data')
+print('data folder = {}'.format(HOME_DATA_FOLDER))
 DATASET_FOLDER = join(HOME_DATA_FOLDER, 'dataset')
+print('hotpotqa data folder = {}'.format(DATASET_FOLDER))
 MODEL_FOLDER = join(HOME_DATA_FOLDER, 'models')
+print('pretrained model folder = {}'.format(MODEL_FOLDER))
 KNOWLEDGE_FOLDER = join(HOME_DATA_FOLDER, 'knowledge')
+print('knowledge folder = {}'.format(KNOWLEDGE_FOLDER))
 OUTPUT_FOLDER = join(HOME_DATA_FOLDER, 'outputs')
+print('output result folder = {}'.format(OUTPUT_FOLDER))
 PRETRAINED_MODEL_FOLDER = join(HOME_DATA_FOLDER, 'models/pretrained')
-
+print('pretrained model with finetuned folder = {}'.format(PRETRAINED_MODEL_FOLDER))
 os.environ['PYTORCH_PRETRAINED_BERT_CACHE'] = join(HOME_DATA_FOLDER, 'models', 'pretrained_cache')
