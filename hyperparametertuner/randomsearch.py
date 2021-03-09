bf2f043b80754a595d9ea7115ff20d9e313cb351
guangtao.wang <guangtao.wang@jd.com>
2021-03-08 17:37:32 -0800

    save model

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -43,7 +43,7 @@ def rand_search_parameter(space: dict):
         raise ValueError('Training batch mode %s not supported' % para_type)
 
 def HypeParameterSpace():
-    learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [1e-5, 5e-6]}
+    learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [1e-5, 2e-6]}
     per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2]}
     gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [1, 2]}
     sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [4, 5, 8]}
@@ -52,13 +52,13 @@ def HypeParameterSpace():
     bi_attn_drop = {'name': 'bi_attn_drop', 'type': 'choice', 'values': [0.3]}
     trans_drop = {'name': 'trans_drop', 'type': 'choice', 'values': [0.3]}
     lstm_drop = {'name': 'lstm_drop', 'type': 'choice', 'values': [0.3]}
-    num_train_epochs = {'name': 'num_train_epochs', 'type': 'choice', 'values': [6]}
+    num_train_epochs = {'name': 'num_train_epochs', 'type': 'choice', 'values': [4]}
     devf_type = {'name': 'devf_type', 'type': 'choice', 'values': ['hgn_low']}
     daug_type = {'name': 'daug_type', 'type': 'choice', 'values': ['hgn_low']}
     model_type = {'name': 'model_type', 'type': 'choice', 'values': ['roberta']}
-    ctx_attn_hidden_dim = {'name': 'ctx_attn_hidden_dim', 'type': 'choice', 'values': [150]} # 300
-    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [150]} # 300
-    gnn = {'name': 'gnn', 'type': 'choice', 'values': ['gat:1,2']} ##'gat:1,2'
+    ctx_attn_hidden_dim = {'name': 'ctx_attn_hidden_dim', 'type': 'choice', 'values': [512]} # 300
+    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [512]} # 300
+    gnn = {'name': 'gnn', 'type': 'choice', 'values': ['gat:1,4']} ##'gat:1,2'
     fine_tuned_encoder = {'name': 'fine_tuned_encoder', 'type': 'choice', 'values': ['ahotrod/roberta_large_squad2']}
     encoder_name_or_path = {'name': 'encoder_name_or_path', 'type': 'choice', 'values': ['roberta-large']}
     #++++++++++++++++++++++++++++++++++
@@ -106,4 +106,4 @@ def generate_random_search_bash(task_num, seed=42, lightning=False):
     print('{} jobs have been generated'.format(task_num))
 
 if __name__ == '__main__':
-    generate_random_search_bash(task_num=2, seed=70)
+    generate_random_search_bash(task_num=4, seed=100)
