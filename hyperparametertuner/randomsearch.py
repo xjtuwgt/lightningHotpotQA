bc6f83213512ac60d936043380cc073d64f42b6d
guangtao.wang <guangtao.wang@jd.com>
2021-03-09 19:07:30 -0800

    save model

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -106,4 +106,4 @@ def generate_random_search_bash(task_num, seed=42, lightning=False):
     print('{} jobs have been generated'.format(task_num))
 
 if __name__ == '__main__':
-    generate_random_search_bash(task_num=2, seed=100)
+    generate_random_search_bash(task_num=5, seed=200)
