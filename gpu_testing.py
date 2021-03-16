233cf4f745fd9531ac6b6849805f33cbd41a6d8f
guangtao.wang <guangtao.wang@jd.com>
2021-03-16 16:34:11 -0700

    save model

--- gpu_testing.py
+++ gpu_testing.py
@@ -1,1 +1,9 @@
-from utils.jdutils import
+from utils.gpu_utils import gpu_id_setting
+import torch
+
+if torch.cuda.is_available():
+    gpus_count = torch.cuda.device_count()
+    gpu_id_list, true_gpus_count = gpu_id_setting(gpus=gpus_count)
+    print('Number of available gpus = {} with device id = {}'.format(true_gpus_count, gpu_id_list))
+else:
+    print('cuda is not available')
