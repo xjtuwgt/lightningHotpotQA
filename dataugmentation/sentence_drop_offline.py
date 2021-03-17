a7c37dddd7d603652ef186d52ed2a2bdf19f9fd3
guangtao.wang <guangtao.wang@jd.com>
2021-03-17 14:44:21 -0700

    save model

--- dataugmentation/sentence_drop_offline.py
+++ dataugmentation/sentence_drop_offline.py
@@ -48,7 +48,7 @@ def support_sentence_drop_out(title, sentence_list, support_fact_ids, drop_out):
 def no_support_sentence_drop_out(title, sentence_list, drop_out):
     sent_num = len(sentence_list)
     sample_size = math.floor(sent_num * drop_out)
-    # print(sent_num)
+    print(sent_num)
     if sample_size < 1:
         return None
     drop_sent_ids = np.random.choice(sent_num, sample_size, replace=False).tolist()
