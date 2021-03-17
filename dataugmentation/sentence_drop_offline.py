704c27f2840747ae7930a95262af23431161be67
guangtao.wang <guangtao.wang@jd.com>
2021-03-17 14:22:03 -0700

    save model

--- dataugmentation/sentence_drop_offline.py
+++ dataugmentation/sentence_drop_offline.py
@@ -1,14 +1,73 @@
 import json
 import sys
+import argparse
+import numpy as np
+import math
+
 
 from tqdm import tqdm
 assert len(sys.argv) == 3
-raw_data = json.load(open(sys.argv[1], 'r')) ## original json data
-#################################
-sentence_drop_output_file = sys.argv[2]
-################################
-selected_para_dict_reverse = {}
-################################
-for case in tqdm(raw_data):
-    guid = case['_id']
-    ##############################################
+
+def hotpot_qa_sentnece_drop_examples(full_file, drop_out: float):
+    with open(full_file, 'r', encoding='utf-8') as reader:
+        full_data = json.load(reader)
+
+    drop_out_cases = []
+    for case in tqdm(full_data):
+        guid = case['_id']
+        sup_facts = list(set([(sp[0], sp[1]) for sp in case['supporting_facts']]))
+        sup_fact_dict = {}
+        for sp in sup_facts:
+            if sp[0] not in sup_fact_dict:
+                sup_fact_dict[sp[0]] = [sp[1]]
+            else:
+                sup_fact_dict[sp[0]].append(sp[1])
+        for key, value in sup_fact_dict.items():
+            sup_fact_dict[key] = sorted(value)
+
+        context = dict(case['context'])
+        ##############################################
+        print(case)
+
+def sentence_drop_context(context, supp_fact_dict: dict, drop_out: float):
+    drop_context = []
+    for ctx_idx, ctx in enumerate(context):
+        title_i, sentences_i = ctx
+        if title_i not in supp_fact_dict:
+            no_support_sentence_drop_out(title=title_i, sentence_list=sentences_i, drop_out=drop_out)
+    return
+
+def support_sentence_drop_out(title, sentence_list, drop_out, support_fact_ids):
+
+    return
+
+
+def no_support_sentence_drop_out(title, sentence_list, drop_out):
+    sent_num = len(sentence_list)
+    sample_size = math.floor(sent_num * drop_out)
+    if sample_size < 1:
+        return None
+    drop_sent_ids = np.random.choice(sent_num, sample_size, replace=False).tolist()
+    sent_keep_list = [sent_ for sent_idx, sent_ in enumerate(sentence_list) if sent_idx not in drop_sent_ids]
+    res = [title, sent_keep_list]
+    return res
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Required parameters
+    parser.add_argument("--full_data", type=str, required=True)
+    parser.add_argument("--full_data_name", type=str, required=True)
+    parser.add_argument("--output_path", type=str, required=True, help='define output data set')
+    parser.add_argument("--drop_out", type=float, default=0.5, help='define dropout ratio')
+
+    args = parser.parse_args()
+    for key, value in vars(args).items():
+        print('Parameter {}: {}'.format(key, value))
+
+    raw_data_file = args.full_data
+    out_put_file = args.output_data
+    drop_out_ratio = args.drop_out
+    hotpot_qa_sentnece_drop_examples(full_file=raw_data_file, drop_out=drop_out_ratio)
---
+++ run_sentdrop.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# DEFINE data related (please make changes according to your configurations)
+# DATA ROOT folder where you put data files
+DATA_ROOT=./data/
+
+
+preprocess() {
+    INPUTS=("hotpot_train_v1.1.json;sent_drop_train")
+    for input in ${INPUTS[*]}; do
+        INPUT_FILE_NAME=$(echo $input | cut -d ";" -f 1)
+        DATA_TYPE=$(echo $input | cut -d ";" -f 2)
+
+        echo "Processing input_file: ${INPUT_FILE}"
+
+        INPUT_FILE=$DATA_ROOT/dataset/data_raw/$INPUT_FILE_NAME
+        OUTPUT_PROCESSED=$DATA_ROOT/dataset/data_raw/$DATA_TYPE
+
+        [[ -d $OUTPUT_PROCESSED ]] || mkdir -p $OUTPUT_PROCESSED
+
+
+        echo "1. Sentence drop based data augmentation"
+        # Input: INPUT_FILE
+        # Output: sent_drop_json
+        python dataugmentation/sentence_drop_offline.py --full_data $INPUT_FILE --output_data OUTPUT_PROCESSED --full_data_name $INPUT_FILE_NAME --drop_out 0.5
+
+        done
+
+}
+
+for proc in  "preprocess"
+do
+    $proc
+done
