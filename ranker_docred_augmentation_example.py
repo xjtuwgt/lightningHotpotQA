942ac233ce9becfd61f47052038909ee31ee4425
guangtao.wang <guangtao.wang@jd.com>
2021-03-18 14:08:55 -0700

    save model

--- ranker_docred_augmentation_example.py
+++ ranker_docred_augmentation_example.py
@@ -16,20 +16,20 @@ if __name__ == '__main__':
     data_folder = args.data_path
     for key, value in vars(args).items():
         print('Parameter {}: {}'.format(key, value))
-    print('1. combine hgn, long, docred')
-    combined_tag_type_pair_list_hgn_long_docred = [('train', 'hgn_low'), ('train', 'long_low'), ('docred', 'docred_low')]
-    hgn_long_docred_combined_type = 'hgn_long_docred_low'
-    features, examples, graphs = combine_data_graph_feat_examples(data_folder=data_folder, config=args, tag_f_type_list=combined_tag_type_pair_list_hgn_long_docred)
-    save_data_graph_feat(out_folder=join(data_folder, 'train'), config=args, f_type=hgn_long_docred_combined_type,
-                         examples=examples, features=features, graphs=graphs)
+    # print('1. combine hgn, long, docred')
+    # combined_tag_type_pair_list_hgn_long_docred = [('train', 'hgn_low'), ('train', 'long_low'), ('docred', 'docred_low')]
+    # hgn_long_docred_combined_type = 'hgn_long_docred_low'
+    # features, examples, graphs = combine_data_graph_feat_examples(data_folder=data_folder, config=args, tag_f_type_list=combined_tag_type_pair_list_hgn_long_docred)
+    # save_data_graph_feat(out_folder=join(data_folder, 'train'), config=args, f_type=hgn_long_docred_combined_type,
+    #                      examples=examples, features=features, graphs=graphs)
 
-    # print('2. combine hgn, long, docred sae')
-    # combined_tag_type_pair_list_hgn_long_docred_sae = [('train', 'hgn_low_sae'), ('train', 'long_low_sae'), ('docred', 'docred_low_sae')]
-    # hgn_docred_combined_type = 'hgn_long_docred_low_sae'
-    # features, examples, graphs = combine_data_graph_feat_examples(data_folder=data_folder, config=args,
-    #                                                               tag_f_type_list=combined_tag_type_pair_list_hgn_long_docred_sae)
-    # # save_data_graph_feat(out_folder=join(data_folder, 'train'), config=args, f_type=hgn_docred_combined_type,
-    # #                      examples=examples, features=features, graphs=graphs)
+    print('2. combine hgn, long, docred sae')
+    combined_tag_type_pair_list_hgn_long_docred_sae = [('train', 'hgn_low_sae'), ('train', 'long_low_sae'), ('docred', 'docred_low_sae')]
+    hgn_long_docred_sae_combined_type = 'hgn_long_docred_low_sae'
+    features, examples, graphs = combine_data_graph_feat_examples(data_folder=data_folder, config=args,
+                                                                  tag_f_type_list=combined_tag_type_pair_list_hgn_long_docred_sae)
+    save_data_graph_feat(out_folder=join(data_folder, 'train'), config=args, f_type=hgn_long_docred_sae_combined_type,
+                         examples=examples, features=features, graphs=graphs)
 
 
 
