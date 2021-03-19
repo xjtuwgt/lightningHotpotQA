819d5b506006a0444d0b0222a3713a03e0f93a02
guangtao.wang <guangtao.wang@jd.com>
2021-03-18 19:15:33 -0700

    save model

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -56,10 +56,10 @@ def HypeParameterSpace():
     trans_drop = {'name': 'trans_drop', 'type': 'choice', 'values': [0.3]}
     lstm_drop = {'name': 'lstm_drop', 'type': 'choice', 'values': [0.3]}
     num_train_epochs = {'name': 'num_train_epochs', 'type': 'choice', 'values': [5]}
-    devf_type = {'name': 'devf_type', 'type': 'choice', 'values': ['hgn_low_sae']}
-    daug_type = {'name': 'daug_type', 'type': 'choice', 'values': ['hgn_docred_low_sae']}
+    devf_type = {'name': 'devf_type', 'type': 'choice', 'values': ['hgn_low']}
+    daug_type = {'name': 'daug_type', 'type': 'choice', 'values': ['hgn_docred_low']}
     model_type = {'name': 'model_type', 'type': 'choice', 'values': ['roberta']}
-    num_edge_type = {'name': 'num_edge_type', 'type': 'choice', 'values': [9]} # if SAE, then this number should be 9
+    num_edge_type = {'name': 'num_edge_type', 'type': 'choice', 'values': [8]} # if SAE, then this number should be 9
     ctx_attn_hidden_dim = {'name': 'ctx_attn_hidden_dim', 'type': 'choice', 'values': [300]} # 300
     hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [300]} # 300
     learning_rate_schema = {'name': 'learning_rate_schema', 'type': 'choice', 'values': ['fixed']}
