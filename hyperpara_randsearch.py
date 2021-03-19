04c228ab7e5499ef0fc1ec83307749fc2f7dfac5
guangtao.wang <guangtao.wang@jd.com>
2021-03-18 17:17:56 -0700

    save model

--- hyperpara_randsearch.py
+++ hyperpara_randsearch.py
@@ -4,7 +4,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Required parameters
-    parser.add_argument("--rand_seed", type=int, required=True)
-    parser.add_argument("--task_num", type=int, required=True)
+    parser.add_argument("--rand_seed", type=int, default=1000)
+    parser.add_argument("--task_num", type=int, default=5)
     args = parser.parse_args()
     generate_random_search_bash(task_num=args.task_num, seed=args.rand_seed)
