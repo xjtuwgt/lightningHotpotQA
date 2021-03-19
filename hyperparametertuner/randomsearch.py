1bd3f138481ea504c78e6c490f5f03137b29326a
guangtao.wang <guangtao.wang@jd.com>
2021-03-18 17:28:53 -0700

    save model

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -19,7 +19,7 @@ def single_task_trial(search_space: dict, rand_seed=42):
         parameter_dict[key] = rand_search_parameter(value)
     parameter_dict['seed'] = rand_seed
     exp_name = 'train.graph.' + parameter_dict['model_type'] + '.bs' + str(parameter_dict['per_gpu_train_batch_size']) + '.as' + str(parameter_dict['gradient_accumulation_steps']) + \
-               '.lr' + str(parameter_dict['learning_rate']) +'.lrs' + str(parameter_dict['learning_rate_schema']) + '.gnn' + str(parameter_dict['gnn']) +\
+               '.lr' + str(parameter_dict['learning_rate']) +'.lrs' + str(parameter_dict['learning_rate_schema']) + '.gnn' + str(parameter_dict['gnn'].replace(':','').replace(',', '.')) +\
                '.data' +str(parameter_dict['daug_type']) + '.seed' +str(rand_seed)
     parameter_dict['exp_name'] = exp_name
     return parameter_dict
@@ -93,7 +93,7 @@ def generate_random_search_bash(task_num, seed=42):
     for i in range(task_num):
         rand_hype_dict = single_task_trial(search_space, seed+i)
         config_json_file_name = 'train.graph.' + rand_hype_dict['model_type'] + '.data.' + rand_hype_dict['daug_type'] \
-                                +'.lr.'+ str(rand_hype_dict['learning_rate']) + '.' + rand_hype_dict['learning_rate_schema'] + '.' + rand_hype_dict['gnn'] \
+                                +'.lr.'+ str(rand_hype_dict['learning_rate']) + '.' + rand_hype_dict['learning_rate_schema'] + '.' + rand_hype_dict['gnn'].replace(',', '.').replace(':', '') \
                                 + str(rand_hype_dict['seed']) + '.json'
         with open(os.path.join(bash_save_path, config_json_file_name), 'w') as fp:
             json.dump(rand_hype_dict, fp)
