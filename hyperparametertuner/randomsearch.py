6de653719f536633e17b28b0dde0518543a62eff
guangtao.wang <guangtao.wang@jd.com>
2021-03-18 17:21:01 -0700

    save model

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -93,7 +93,7 @@ def generate_random_search_bash(task_num, seed=42):
     for i in range(task_num):
         rand_hype_dict = single_task_trial(search_space, seed+i)
         config_json_file_name = 'train.graph.' + rand_hype_dict['model_type'] + '.data.' + rand_hype_dict['daug_type'] \
-                                +'.lr.'+ str(rand_hype_dict['learning_rate']) + '.' + rand_hype_dict['learning_rate_schema']\
+                                +'.lr.'+ str(rand_hype_dict['learning_rate']) + '.' + rand_hype_dict['learning_rate_schema'] + '.' + rand_hype_dict['gnn'] +\
                                 + str(rand_hype_dict['seed']) + '.json'
         with open(os.path.join(bash_save_path, config_json_file_name), 'w') as fp:
             json.dump(rand_hype_dict, fp)
