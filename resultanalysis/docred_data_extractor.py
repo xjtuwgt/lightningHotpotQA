2de24f31ebf5753397c06c85e9e601756245fc1e
guangtao.wang <guangtao.wang@jd.com>
2021-03-23 12:07:44 -0700

    save model

--- resultanalysis/docred_data_extractor.py
+++ resultanalysis/docred_data_extractor.py
@@ -5,10 +5,10 @@ from tqdm import tqdm
 
 
 def docred_checker():
-    DOCRED_OUTPUT_PROCESSED_fill_file = join(DATASET_FOLDER, 'dataset/data_processed/docred_multihop_para.json')
+    DOCRED_OUTPUT_PROCESSED_fill_file = join(DATASET_FOLDER, 'data_processed/docred_multihop_para.json')
     with open(DOCRED_OUTPUT_PROCESSED_fill_file, 'r', encoding='utf-8') as reader:
         full_data = json.load(reader)
-
+    print('loading data from {}'.format(DOCRED_OUTPUT_PROCESSED_fill_file))
     examples = []
     for case in tqdm(full_data):
         print(case)
