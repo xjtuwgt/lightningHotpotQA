26371bfeccbc941dcc6b7c57afe92084c1c767a5
guangtao.wang <guangtao.wang@jd.com>
2021-03-23 12:23:54 -0700

    save model

--- resultanalysis/docred_data_extractor.py
+++ resultanalysis/docred_data_extractor.py
@@ -5,11 +5,14 @@ from tqdm import tqdm
 
 
 def docred_checker():
-    DOCRED_OUTPUT_PROCESSED_fill_file = join(DATASET_FOLDER, 'data_processed/docred/docred_multihop_para.json')
-    # DOCRED_OUTPUT_PROCESSED_fill_file = join(DATASET_FOLDER, 'data_raw/converted_docred_total.json')
-    with open(DOCRED_OUTPUT_PROCESSED_fill_file, 'r', encoding='utf-8') as reader:
-        full_data = json.load(reader)
-    print('loading {} data from {}'.format(len(full_data), DOCRED_OUTPUT_PROCESSED_fill_file))
+    DOCRED_OUTPUT_PROCESSED_para_file = join(DATASET_FOLDER, 'data_processed/docred/docred_multihop_para.json')
+    DOCRED_OUTPUT_PROCESSED_raw_file = join(DATASET_FOLDER, 'data_raw/converted_docred_total.json')
+    with open(DOCRED_OUTPUT_PROCESSED_raw_file, 'r', encoding='utf-8') as reader:
+        raw_data = json.load(reader)
+    with open(DOCRED_OUTPUT_PROCESSED_para_file, 'r', encoding='utf-8') as reader:
+        para_data = json.load(reader)
+    print('loading {} data from {}'.format(len(raw_data), DOCRED_OUTPUT_PROCESSED_raw_file))
     examples = []
-    for case in tqdm(full_data):
+    for case in tqdm(raw_data):
         print(case)
+        print(para_data[case['_id']])
