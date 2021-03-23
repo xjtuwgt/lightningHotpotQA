fc01726d82d796ba66a75468e5113d876080d91d
guangtao.wang <guangtao.wang@jd.com>
2021-03-23 14:27:35 -0700

    save model

--- resultanalysis/docred_data_extractor.py
+++ resultanalysis/docred_data_extractor.py
@@ -29,7 +29,7 @@ def add_space(context_list):
 
 def docred_checker():
     DOCRED_OUTPUT_PROCESSED_para_file = join(DATASET_FOLDER, 'data_processed/docred/docred_multihop_para.json')
-    DOCRED_OUTPUT_PROCESSED_raw_file = join(DATASET_FOLDER, 'data_raw/space_converted_docred_total.json') #converted_docred_total.json
+    DOCRED_OUTPUT_PROCESSED_raw_file = join(DATASET_FOLDER, 'data_raw/converted_docred_total.json') #converted_docred_total.json
     # Saved_raw_DOCRED_OUTPUT_PROCESSED = join(DATASET_FOLDER, 'data_raw/space_converted_docred_total.json')
     with open(DOCRED_OUTPUT_PROCESSED_raw_file, 'r', encoding='utf-8') as reader:
         raw_data = json.load(reader)
