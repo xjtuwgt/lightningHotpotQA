9c6b5492ac66ccef33963c7432115e3fd2208bc2
guangtao.wang <guangtao.wang@jd.com>
2021-03-24 10:29:21 -0700

    save model

--- PredictionErrorAnalysis.py
+++ PredictionErrorAnalysis.py
@@ -23,10 +23,10 @@ if __name__ == '__main__':
 
     # Other parameters
     parser.add_argument("--model_type", default="roberta", type=str)
-    parser.add_argument("--model_name_or_path", default='train.roberta.bs2.lr1e-05.datahgn_low.seed304', type=str,
+    parser.add_argument("--model_name_or_path", default='train.graph.roberta.bs2.as4.lr1e-05.lrsfixed.gnngat1.4.datahgn_docred_low.seed667', type=str,
                         help="Path to pre-trained model")
 
-    parser.add_argument("--pred_res_name", default='pred.epoch_5.step_33918.json', type=str, help="Prediction result")
+    parser.add_argument("--pred_res_name", default='pred.epoch_5.json', type=str, help="Prediction result")
 
     parser.add_argument("--max_entity_num", default=60, type=int)
     parser.add_argument("--max_sent_num", default=40, type=int)
