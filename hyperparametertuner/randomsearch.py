362afc89e3ae2f2ed1d4ebc40cadcbf6436d77e8
guangtao.wang <guangtao.wang@jd.com>
2021-03-23 17:20:52 -0700

    save model

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -48,7 +48,7 @@ def HypeParameterSpace():
     learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [3e-5, 5e-5, 1e-4, 1.5e-4]}
     layer_wise_lr_decay = {'name': 'layer_wise_lr_decay', 'type': 'choice', 'values': [0.7, 0.8, 0.9]}
     per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2]}
-    gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [2]}
+    gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [2,4]}
     sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [4, 5]} ##
     frozen_layer_num = {'name': 'frozen_layer_number', 'type': 'choice', 'values': [0]}
     gnn_drop = {'name': 'gnn_drop', 'type': 'choice', 'values': [0.3]} #0.3
