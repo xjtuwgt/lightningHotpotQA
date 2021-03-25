c8d7dca80e707d17c29b1c1aed78f1bbb67fba28
guangtao.wang <guangtao.wang@jd.com>
2021-03-25 09:09:36 -0700

    save model

--- best_para_setting.py
+++ best_para_setting.py
@@ -1,4 +1,4 @@
 from resultanalysis.metric_collector import best_metric_collection
 
 if __name__ == '__main__':
-    best_metric_collection(key_word='hgn_long_docred_low_sae')
+    best_metric_collection(key_word='sae')
