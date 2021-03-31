5e1a4473b4bbf528bb6b4a828e51f53d13de0558
guangtao.wang <guangtao.wang@jd.com>
2021-03-31 10:35:10 -0700

    save model

--- utils/jdevalUtil.py
+++ utils/jdevalUtil.py
@@ -79,6 +79,8 @@ def jd_eval_model(args, encoder, model, dataloader, example_dict, feature_dict, 
                                                                                     yp2.data.cpu().numpy().tolist(),
                                                                                     type_prob)
         ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        print(ent.shape)
+        print(batch['ans_cand_mask'])
         x, y, z = convert_answer_to_sent_paras(example_dict, feature_dict, batch,
                                                                                     yp1.data.cpu().numpy().tolist(),
                                                                                     yp2.data.cpu().numpy().tolist(),
