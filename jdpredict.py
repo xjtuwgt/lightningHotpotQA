42e3ecf1b0d5ffb4d066bff22e35b210f721d902
guangtao.wang <guangtao.wang@jd.com>
2021-03-30 20:56:08 -0700

    save model

--- jdpredict.py
+++ jdpredict.py
@@ -102,12 +102,12 @@ model.eval()
 output_pred_file = join(args.exp_name, 'dev_pred.json')
 output_eval_file = join(args.exp_name, 'dev_eval.txt')
 
-# metrics, threshold = jd_eval_model(args, encoder, model,
-#                                 dev_dataloader, dev_example_dict, dev_feature_dict,
-#                                 output_pred_file, output_eval_file, args.dev_gold_file)
-metrics, threshold = eval_model(args, encoder, model,
+metrics, threshold = jd_eval_model(args, encoder, model,
                                 dev_dataloader, dev_example_dict, dev_feature_dict,
                                 output_pred_file, output_eval_file, args.dev_gold_file)
+# metrics, threshold = eval_model(args, encoder, model,
+#                                 dev_dataloader, dev_example_dict, dev_feature_dict,
+#                                 output_pred_file, output_eval_file, args.dev_gold_file)
 print("Best threshold: {}".format(threshold))
 for key, val in metrics.items():
     print("{} = {}".format(key, val))
