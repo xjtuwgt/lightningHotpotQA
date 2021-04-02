a5f8de5ae2894d81d9cd0bc4d74a61471b917923
guangtao.wang <guangtao.wang@jd.com>
2021-04-02 09:15:30 -0700

    save model

--- resultanalysis/errorAnalysis.py
+++ resultanalysis/errorAnalysis.py
@@ -418,7 +418,7 @@ def error_analysis_question_type(raw_data, predictions, tokenizer, use_ent_ans=F
 def prediction_score_analysis(raw_data, predictions, prediction_scores):
     def positive_neg_score(scores, mask, names, gold_names, pred_names):
         assert len(scores) == len(mask)
-
+        print(type(scores), type(mask))
         return
 
     for row in raw_data:
