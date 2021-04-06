788f1d4f2e01b5ddac191b6d4d8a6adfdf88bc78
guangtao.wang <guangtao.wang@jd.com>
2021-04-06 11:14:46 -0700

    save model

--- download_model.sh
+++ download_model.sh
@@ -31,7 +31,7 @@ albert() {
   wget -P $DATA_ROOT/models/pretrained/mfeb/albert-xxlarge-v2-squad2 $ALBERT_SQUAD2/special_tokens_map.json
 }
 
-ELECTA_SQUAD1=https://huggingface.co/mrm8488/electra-large-finetuned-squadv1/blob/main/
+ELECTA_SQUAD1=https://huggingface.co/mrm8488/electra-large-finetuned-squadv1/blob/main
 electra() {
   [[ -d $DATA_ROOT ]] || mkdir -p $DATA_ROOT/models/pretrained/mrm8488/electra-large-finetuned-squadv1
 
@@ -41,7 +41,8 @@ electra() {
   wget -P $DATA_ROOT/models/pretrained/mrm8488/electra-large-finetuned-squadv1 $ELECTA_SQUAD1/special_tokens_map.json
 }
 
-for proc in "roberta" "albert" "electra"
+#for proc in "roberta" "albert" "electra"
+for proc in "electra"
 do
     $proc
 done
