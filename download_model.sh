e64dee4362ce10047aa9de6d1f8f06c2078ef460
guangtao.wang <guangtao.wang@jd.com>
2021-04-06 11:02:57 -0700

    save model

--- download_model.sh
+++ download_model.sh
@@ -31,7 +31,17 @@ albert() {
   wget -P $DATA_ROOT/models/pretrained/mfeb/albert-xxlarge-v2-squad2 $ALBERT_SQUAD2/special_tokens_map.json
 }
 
-for proc in "roberta" "albert"
+ELECTA_SQUAD1=https://huggingface.co/mrm8488/electra-large-finetuned-squadv1/blob/main/
+electra() {
+  [[ -d $DATA_ROOT ]] || mkdir -p $DATA_ROOT/models/pretrained/mrm8488/electra-large-finetuned-squadv1
+
+  wget -P $DATA_ROOT/models/pretrained/mrm8488/electra-large-finetuned-squadv1 $ELECTA_SQUAD1/config.json
+  wget -P $DATA_ROOT/models/pretrained/mrm8488/electra-large-finetuned-squadv1 $ELECTA_SQUAD1/tokenizer_config.json
+  wget -P $DATA_ROOT/models/pretrained/mrm8488/electra-large-finetuned-squadv1 $ELECTA_SQUAD1/pytorch_model.bin
+  wget -P $DATA_ROOT/models/pretrained/mrm8488/electra-large-finetuned-squadv1 $ELECTA_SQUAD1/special_tokens_map.json
+}
+
+for proc in "roberta" "albert" "electra"
 do
     $proc
 done
