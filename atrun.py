1ec4e0932a7733e7babe4116398e36b89ef7b8bb
guangtao.wang <guangtao.wang@jd.com>
2021-04-07 16:29:38 -0700

    save model

--- atrun.py
+++ atrun.py
@@ -1,16 +1,21 @@
 import numpy as np
 from adaptive_threshold.atutils import distribution_feat, distribution_feat_extraction, parse_args
 from os.path import join
+import json
 
 if __name__ == '__main__':
 
-    x = np.random.random(10)
-    print(x)
-    y = distribution_feat_extraction(scores=x, keep_num=True)
-    print(len(y))
-    z = distribution_feat(scores=x)
-    print(len(z))
+    # x = np.random.random(10)
+    # print(x)
+    # y = distribution_feat_extraction(scores=x, keep_num=True)
+    # print(len(y))
+    # z = distribution_feat(scores=x)
+    # print(len(z))
 
     args = parse_args()
     dev_score_file_name = join(args.pred_dir, args.model_name_or_path, args.dev_score_data)
-    print(dev_score_file_name)
+    pred_score_results_file = join(args.pred_dir, args.model_name_or_path, args.pred_score_name)
+    with open(pred_score_results_file, 'r', encoding='utf-8') as reader:
+        pred_score_data = json.load(reader)
+    print(len(pred_score_data))
+    # print(dev_score_file_name)
