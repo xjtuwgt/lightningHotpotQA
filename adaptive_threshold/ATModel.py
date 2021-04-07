7fe020d2e0caaeae624ea4560bb4c47173caa50e
guangtao.wang <guangtao.wang@jd.com>
2021-04-07 16:23:27 -0700

    save model

--- adaptive_threshold/ATModel.py
+++ adaptive_threshold/ATModel.py
@@ -1,18 +1,54 @@
 from sklearn import linear_model
+from sklearn import ensemble
 from sklearn.metrics import mean_squared_error
+import pickle
 
 def at_lasso_model_train(X, y):
     clf = linear_model.Lasso(alpha=0.1)
     clf.fit(X, y)
     return clf
 
+def at_boostree_model_train(X, y, params):
+    reg = ensemble.GradientBoostingRegressor(**params)
+    reg.fit(X, y)
+    return reg
+
+def save_sklearn_pickle_model(model, pkl_filename):
+    with open(pkl_filename, 'wb') as file:
+        pickle.dump(model, file)
+    print('Saving model in {}'.format(pkl_filename))
+
+def load_sklearn_pickle_model(pkl_filename):
+    with open(pkl_filename, 'rb') as file:
+        pickle_model = pickle.load(file)
+    return pickle_model
+
+
 
 x = [[0, 0], [1, 1], [2, 2], [3, 3]]
 y = [0, 1, 2, 3]
 
-clf = at_lasso_model_train(x, y)
-print(clf)
-print(clf.coef_)
+# clf = at_lasso_model_train(x, y)
+# print(clf)
+# print(clf.coef_)
+#
+# mse = mean_squared_error(y, clf.predict(x))
+# print(mse)
 
-mse = mean_squared_error(y, clf.predict(x))
-print(mse)
+params = {'n_estimators': 1000,
+          'max_depth': 2,
+          'min_samples_split': 3,
+          'learning_rate': 0.01,
+          'verbose': True,
+          'loss': 'ls'}
+
+reg = at_boostree_model_train(X=x, y=y, params=params)
+# pickle_file_name = 'tree.pkl'
+# # save_sklearn_pickle_model(reg, pickle_file_name)
+# # load_reg = load_sklearn_pickle_model(pickle_file_name)
+# #
+# # print(load_reg.predict(x))
+#
+# mse = mean_squared_error(y, reg.predict(x))
+# print("The mean squared error (MSE) on test set: {:.4f}".format(mse))
+
--- adaptive_threshold/adaptiveDatapreprocess.py
+++ adaptive_threshold/adaptiveDatapreprocess.py
@@ -1,5 +1,10 @@
 import argparse
 from envs import OUTPUT_FOLDER, DATASET_FOLDER
+from numpy import ndarray
+import numpy as np
+from tqdm import tqdm
+from os.path import join
+
 def parse_args(args=None):
     parser = argparse.ArgumentParser(
         description='Adaptive threshold prediction')
@@ -14,7 +19,50 @@ def parse_args(args=None):
     parser.add_argument("--model_type", default="roberta", type=str)
     parser.add_argument("--model_name_or_path", default='train.graph.roberta.bs2.as1.lr2e-05.lrslayer_decay.lrd0.9.gnngat1.4.datahgn_docred_low_saeRecAdam.cosine.seed103', type=str,
                         help="Path to pre-trained model")
-    parser.add_argument("--dev_score_data", type=str, default='data_raw/hotpot_dev_distractor_v1.json')
-    parser.add_argument("--train_score_data", type=str, default='data_raw/hotpot_dev_distractor_v1.json')
+    parser.add_argument("--dev_score_data", type=str, default='dev_score.json')
+    parser.add_argument("--train_score_data", type=str, default='train_score.json')
 
     return parser.parse_args(args)
+
+
+def distribution_feat_extraction(scores: ndarray, keep_num=False):
+    ##     min, max, mean, median, 1/4, 3/4 score, std, num
+    ##gap: min, max, mean, median, 1/4, 3/4 score, std
+    min_value, max_value, mean_value, median_value, std_value = np.min(scores), np.max(scores), np.mean(scores), np.median(scores), np.std(scores)
+    quartile_1, quartile_2 = np.percentile(scores, 25), np.percentile(scores, 75)
+    dist_feat = [min_value, max_value, mean_value, median_value, std_value, quartile_1, quartile_2]
+    dist_feat = [x.tolist() for x in dist_feat]
+    if keep_num:
+        num = float(scores.shape[0])
+        dist_feat.append(num)
+    return dist_feat
+
+def distribution_feat(scores: ndarray):
+    dist_feat = distribution_feat_extraction(scores=scores, keep_num=True)
+    if scores.shape[0] > 1:
+        sorted_score = np.sort(scores)
+        reverse_score = sorted_score[::-1]
+        gap_scores = np.array([reverse_score[i] - reverse_score[i+1] for i in range(scores.shape[0]-1)])
+        gap_dist_feat = distribution_feat_extraction(scores=gap_scores, keep_num=False)
+    else:
+        gap_dist_feat = [0.0] * 7
+    dist_feat.extend(gap_dist_feat)
+    return dist_feat
+
+def feat_label_extraction(data):
+    for row_idx, row in tqdm(enumerate(data)):
+        print(row_idx)
+
+if __name__ == '__main__':
+
+    x = np.random.random(10)
+    print(x)
+    y = distribution_feat_extraction(scores=x, keep_num=True)
+    print(len(y))
+    z = distribution_feat(scores=x)
+    print(len(z))
+
+    args = parse_args()
+    dev_score_file_name = join(args.pred_dir, args.model_name_or_path, args.dev_score_data)
+    print(dev_score_file_name)
+
