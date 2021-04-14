0dad26e18d4f05993305a13a505453c65b08aa8c
guangtao.wang <guangtao.wang@jd.com>
2021-04-14 10:16:33 -0700

    save model

--- adaptive_threshold/range_argument_parser.py
+++ adaptive_threshold/range_argument_parser.py
@@ -60,12 +60,12 @@ def train_parser():
     parser.add_argument("--pickle_model_check_point_name", type=str, help='checkpoint name')
     parser.add_argument("--rand_seed", type=int, default=4321)
 
-    parser.add_argument("--train_batch_size", type=int, default=1024, help='training batch size')
+    parser.add_argument("--train_batch_size", type=int, default=4096, help='training batch size')
     parser.add_argument("--eval_batch_size", type=int, default=256, help='evaluation batch size')
 
     parser.add_argument("--cls_emb_dim", type=int, default=300, help='cls_emb_dim')
     parser.add_argument("--emb_dim", type=int, default=345, help='cls_emb_dim')
-    parser.add_argument("--hid_dim", type=int, default=256, help='cls_emb_dim')
+    parser.add_argument("--hid_dim", type=int, default=128, help='cls_emb_dim')
 
     parser.add_argument("--train_filter", type=bool, default=False, help='whether performing train filter')
     parser.add_argument("--cpu_number", type=int, default=12, help='cpu number')
