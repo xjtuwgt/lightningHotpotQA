065cdef8da3292c04498d3f37da2f6bd5bd74291
guangtao.wang <guangtao.wang@jd.com>
2021-04-13 17:39:45 -0700

    save model

--- adaptive_threshold/RangeModel.py
+++ adaptive_threshold/RangeModel.py
@@ -1,6 +1,23 @@
 import torch
+from torch import Tensor as T
+from torch import nn
 
 
-class RangeModel(torch.nn.Module):
-    def __init__(self):
+class RangeModel(nn.Module):
+    def __init__(self, args):
         super(RangeModel, self).__init__()
+        self.args = args
+        self.cls_emb_dim = self.args.cls_emb_dim
+        self.emb_dim = self.args.emb_dim
+        self.score_dim = self.emb_dim - self.cls_emb_dim
+        self.hid_dim = self.args.hid_dim
+
+
+
+
+    def forward(self, x: T):
+        assert x.shape[1] == self.emb_dim
+        cls_x = x[:,:self.cls_emb_dim]
+        score_x = x[:,self.cls_emb_dim:]
+
+        return
---
+++ adaptive_threshold/range_argument_parser.py
@@ -0,0 +1,68 @@
+# coding=utf-8
+#/usr/bin/env python3
+import os
+import argparse
+import torch
+import json
+import logging
+import random
+import numpy as np
+from os.path import join
+from utils.gpu_utils import gpu_id_setting
+
+from envs import DATASET_FOLDER, OUTPUT_FOLDER, PRETRAINED_MODEL_FOLDER
+
+logger = logging.getLogger(__name__)
+
+
+def boolean_string(s):
+    if s.lower() not in {'false', 'true'}:
+        raise ValueError('Not a valid boolean string')
+    return s.lower() == 'true'
+
+def json_to_argv(json_file):
+    j = json.load(open(json_file))
+    argv = []
+    for k, v in j.items():
+        new_v = str(v) if v is not None else None
+        argv.extend(['--' + k, new_v])
+    return argv
+
+
+def train_parser():
+    parser = argparse.ArgumentParser(
+        description='Adaptive threshold prediction')
+    ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    parser.add_argument("--raw_dev_data", type=str, default='data_raw/hotpot_dev_distractor_v1.json')
+    parser.add_argument("--raw_train_data", type=str, default='data_raw/hotpot_train_v1.1.json')
+    parser.add_argument("--input_dir", type=str, default=DATASET_FOLDER, help='define output directory')
+    parser.add_argument("--output_dir", type=str, default=OUTPUT_FOLDER, help='define output directory')
+    parser.add_argument("--pred_dir", type=str, default=OUTPUT_FOLDER, help='define output directory')
+    # Other parameters
+    parser.add_argument("--train_type", type=str, default='hgn_low_sae', help='data type')
+    parser.add_argument("--model_name_or_path",
+                        default='train.graph.roberta.bs2.as1.lr2e-05.lrslayer_decay.lrd0.9.gnngat1.4.datahgn_docred_low_saeRecAdam.cosine.seed103',
+                        type=str,
+                        help="Path to pre-trained model")
+    parser.add_argument("--dev_score_name", type=str, default='dev_score.json')
+    parser.add_argument("--train_score_name", type=str, default='train_score.json')
+
+    parser.add_argument("--dev_feat_name", type=str, default='dev_np_data.npz')
+    parser.add_argument("--dev_feat_class_name", type=str, default='dev_class_np_data.npz')
+    parser.add_argument("--dev_feat_json_name", type=str, default='dev_json_data.json')
+    parser.add_argument("--train_feat_name", type=str, default='train_np_data.npz')
+    parser.add_argument("--train_feat_class_name", type=str, default='train_class_np_data.npz')
+    parser.add_argument("--class_label_map_name", type=str, default='class_label_dict.json')
+    parser.add_argument("--pred_threshold_json_name", type=str, default='pred_thresholds.json')
+
+    parser.add_argument("--pickle_model_name", type=str, default='at_pred_model.pkl')
+    parser.add_argument("--pickle_model_check_point_name", type=str, help='checkpoint name')
+    parser.add_argument("--rand_seed", type=int, default=4321)
+
+    parser.add_argument("--train_batch_size", type=int, default=32, help='training batch size')
+    parser.add_argument("--eval_batch_size", type=int, default=32, help='evaluation batch size')
+
+    parser.add_argument("--cls_emb_dim", type=int, default=300, help='cls_emb_dim')
+    parser.add_argument("--emb_dim", type=int, default=345, help='cls_emb_dim')
+    parser.add_argument("--hid_dim", type=int, default=256, help='cls_emb_dim')
