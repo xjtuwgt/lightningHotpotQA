f6425d9106708c4b2ee398b215ecc2390350cceb
guangtao.wang <guangtao.wang@jd.com>
2021-04-21 16:40:59 -0700

    save model

--- HotpotQAModel/hotpotqa_dump_features.py
+++ HotpotQAModel/hotpotqa_dump_features.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import argparse
 from model_envs import MODEL_CLASSES
 from HotpotQAModel.hotpotqaUtils import hotpot_answer_tokenizer
