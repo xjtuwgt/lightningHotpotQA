c866fa150f0db580fb4083ba13639cb3087db996
guangtao.wang <guangtao.wang@jd.com>
2021-04-22 09:51:53 -0700

    save model

--- jdqamodel/hotpotqa_test_features.py
+++ jdqamodel/hotpotqa_test_features.py
@@ -191,13 +191,13 @@ def case_to_feature_checker(para_file: str,
         for ans_idx, ans_span in enumerate(ans_spans):
             # print(ans_span)
             # print(len(doc_input_ids))
-            if ans_span[0] < 0 or ans_span[0] >= len(doc_input_ids):
+            if ans_span[0] < 0 or ans_span[0] >= len(doc_input_ids) or ans_span[1] >= len(doc_input_ids):
                 print(ans_span)
                 print(len(doc_input_ids))
             # ans_inp_ids = doc_input_ids[ans_span[0]:ans_spans[1]]
             # decoded_ans = tokenizer.decode(ans_inp_ids)
             # print('{} Orig\t{}\t{}\t{}'.format(ans_idx, orig_answer, exm_answer, decoded_ans))
-        print('*' * 75)
+        # print('*' * 75)
 
 
 if __name__ == '__main__':
