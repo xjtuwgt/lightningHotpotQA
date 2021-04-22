afb0cdf5f7f7ca372e159092a2ddac0a97782b5f
guangtao.wang <guangtao.wang@jd.com>
2021-04-22 13:54:17 -0700

    save model

--- hotpotqa_run.sh
+++ hotpotqa_run.sh
@@ -45,8 +45,8 @@ preprocess() {
 #        # Output: long_multihop_para.json
 #        python longformerscripts/4_longformer_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json $SELECTEED_DOC_NUM
 
-        echo "2. Tokenized example extraction (Roberta low)"
-        python jdqamodel/hotpotqa_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path roberta-large --do_lower_case --model_type roberta --tokenizer_name roberta-large --output_dir $OUTPUT_FEAT  --ranker long --data_type $DATA_TYPE
+#        echo "2. Tokenized example extraction (Roberta low)"
+#        python jdqamodel/hotpotqa_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path roberta-large --do_lower_case --model_type roberta --tokenizer_name roberta-large --output_dir $OUTPUT_FEAT  --ranker long --data_type $DATA_TYPE
 #
 #        echo "3. Tokenized example extraction (Albert low)"
 #        python jdqamodel/hotpotqa_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path albert-xxlarge-v2 --do_lower_case --model_type albert --tokenizer_name albert-xxlarge-v2 --output_dir $OUTPUT_FEAT  --ranker long --data_type $DATA_TYPE
