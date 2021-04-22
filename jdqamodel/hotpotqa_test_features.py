7eaaf0a437e7ae2a251d03fac3bd49fda8626ef5
guangtao.wang <guangtao.wang@jd.com>
2021-04-22 14:15:16 -0700

    save model

--- jdqamodel/hotpotqa_test_features.py
+++ jdqamodel/hotpotqa_test_features.py
@@ -281,8 +281,8 @@ def sent_drop_case_to_feature_checker(para_file: str,
         # # print(len(doc_input_ids))
         # # print('orig', doc_input_ids)
         # # print(len(sent_spans))
-        # if len(doc_input_ids) > 512:
-        #     larger_512 += 1
+        if len(doc_input_ids) > 512:
+            larger_512 += 1
         # print('orig', example_i.ctx_input_ids)
         drop_example_i = example_sent_drop(case=example_i, drop_ratio=1.0)
         # print('drop', drop_example_i.ctx_input_ids)
@@ -310,13 +310,13 @@ def sent_drop_case_to_feature_checker(para_file: str,
         # print('selected para names: ', sel_para_names)
         # print('example para names: ', example_i.para_names)
 
-        # drop_doc_input_ids, drop_query_spans, drop_para_spans, drop_sent_spans, drop_ans_spans, drop_ans_type_label = \
-        #     case_to_features(case=drop_example_i, train_dev=True)
+        drop_doc_input_ids, drop_query_spans, drop_para_spans, drop_sent_spans, drop_ans_spans, drop_ans_type_label = \
+            case_to_features(case=drop_example_i, train_dev=True)
         # print(len(drop_doc_input_ids))
         # # print('drop', drop_doc_input_ids)
         # print(len(drop_sent_spans))
-        # if len(drop_doc_input_ids) > 512:
-        #     drop_larger_512 += 1
+        if len(drop_doc_input_ids) > 512:
+            drop_larger_512 += 1
 
         # print(type(doc_input_ids), type(query_spans), type(para_spans), type(sent_spans), type(ans_spans))
         # orig_query = row['question']
