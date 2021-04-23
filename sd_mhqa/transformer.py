0131e15b2c152999ed62e8e266eea9834f2d499e
guangtao.wang <guangtao.wang@jd.com>
2021-04-23 15:49:33 -0700

    save model

--- sd_mhqa/transformer.py
+++ sd_mhqa/transformer.py
@@ -64,15 +64,9 @@ class ScaleDotProductAttention(nn.Module):
         d_k = query.size(-1)
         scores = torch.matmul(query, key.transpose(-2, -1)) \
                  / math.sqrt(d_k)
-        print(mask.sum(dim=-1))
-
         if mask is not None:
             scores = scores.masked_fill(mask == 0, -1e9)
-
-
         p_attn = F.softmax(scores, dim=-1)
-        for i in range(mask.shape[0]):
-            print(p_attn[i][0])
         if dropout is not None:
             p_attn = dropout(p_attn)
         return torch.matmul(p_attn, value), p_attn
