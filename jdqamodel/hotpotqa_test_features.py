78a8da6fd1505ff92a9a3ac27aa0f62ca8bf025b
guangtao.wang <guangtao.wang@jd.com>
2021-04-22 22:13:43 -0700

    save model

--- jdqamodel/hotpotqa_test_features.py
+++ jdqamodel/hotpotqa_test_features.py
@@ -604,12 +604,16 @@ def data_loader_checker(para_file: str,
     print('Number of examples = {}'.format(len(examples)))
 
     # hotpotdata = HotpotTestDataset(examples=examples, sep_token_id=tokenizer.sep_token_id)
+    # dev_data_loader = DataLoader(dataset=hotpotdata, batch_size=8,
+    #         shuffle=False,
+    #         num_workers=5,
+    #         collate_fn=HotpotTestDataset.collate_fn)
     hotpotdata = HotpotDataset(examples=examples, sep_token_id=tokenizer.sep_token_id, sent_drop_ratio=-1)
 
     dev_data_loader = DataLoader(dataset=hotpotdata, batch_size=8,
             shuffle=False,
             num_workers=5,
-            collate_fn=HotpotTestDataset.collate_fn)
+            collate_fn=HotpotDataset.collate_fn)
 
     for batch_idx, batch in tqdm(enumerate(dev_data_loader)):
         # print(batch_idx)
