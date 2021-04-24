5224f56b5ff0ecbf8a9679893b091765eb2d0bdd
guangtao.wang <guangtao.wang@jd.com>
2021-04-23 20:58:55 -0700

    save model

--- sd_mhqa/hotpotqa_data_loader.py
+++ sd_mhqa/hotpotqa_data_loader.py
@@ -103,10 +103,10 @@ class HotpotDataset(Dataset):
             if len(trim_ans_spans) == 0:
                 y1, y2 = [IGNORE_INDEX], [IGNORE_INDEX]
             elif len(trim_ans_spans) == 1:
-                y1, y2 = [trim_ans_spans[0][0]], [trim_ans_spans[0][1]]
+                y1, y2 = [trim_ans_spans[0][0]], [trim_ans_spans[0][1]-1]
             else:
                 rand_idx = np.random.randint(len(trim_ans_spans))
-                y1, y2 = [trim_ans_spans[rand_idx][0]], [trim_ans_spans[rand_idx][1]]
+                y1, y2 = [trim_ans_spans[rand_idx][0]], [trim_ans_spans[rand_idx][1]-1]
         q_type = torch.LongTensor(q_type)
         y1 = torch.LongTensor(y1)
         y2 = torch.LongTensor(y2)
