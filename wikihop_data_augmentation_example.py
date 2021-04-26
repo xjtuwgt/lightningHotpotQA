dd17a478ac6da63d0aeba764661ec005011c5054
guangtao.wang <guangtao.wang@jd.com>
2021-04-26 15:30:19 -0700

    save model

--- wikihop_data_augmentation_example.py
+++ wikihop_data_augmentation_example.py
@@ -15,20 +15,45 @@ from envs import DATASET_FOLDER
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Required parameters
-    parser.add_argument("--data_path", type=str, default=join(DATASET_FOLDER, 'data_raw/wikihop_data'))
-    parser.add_argument("--data_name", type=str, default='ner_train.json')
-    parser.add_argument("--output_path", type=str, default=join(DATASET_FOLDER, 'data_processed/wikihop'))
+    # parser.add_argument("--data_path", type=str, default=join(DATASET_FOLDER, 'data_raw/wikihop_data'))
+    # parser.add_argument("--data_name", type=str, default='ner_train.json')
+    # parser.add_argument("--output_path", type=str, default=join(DATASET_FOLDER, 'data_processed/wikihop'))
+    #
+    # args = parser.parse_args()
+    # data_folder = args.data_path
+    # for key, value in vars(args).items():
+    #     print('Parameter {}: {}'.format(key, value))
+    #
+    #
+    #
+    # wiki_ner_data_file_name = join(args.output_path, args.data_name)
+    # # data_stats(wiki_data_name=wiki_ner_data_file_name)
+    # token_data_examples = read_wikihop_examples(full_file=wiki_ner_data_file_name)
+    # wiki_token_ner_data_file_name = join(args.output_path, 'token_' + args.data_name)
+    # with open(wiki_token_ner_data_file_name, 'w') as f:
+    #     json.dump(token_data_examples, f)
 
-    args = parser.parse_args()
-    data_folder = args.data_path
-    for key, value in vars(args).items():
-        print('Parameter {}: {}'.format(key, value))
+    import json
+    from tqdm import tqdm
 
+    dev_data_file_name = join(DATASET_FOLDER, 'data_raw', 'hotpot_dev_distractor_v1.json')
+    test_data_file_name = join(DATASET_FOLDER, 'data_raw', 'hotpot_test_distractor_v1.json')
 
+    with open(dev_data_file_name, 'r', encoding='utf-8') as reader:
+        dev_full_data = json.load(reader)
 
-    wiki_ner_data_file_name = join(args.output_path, args.data_name)
-    # data_stats(wiki_data_name=wiki_ner_data_file_name)
-    token_data_examples = read_wikihop_examples(full_file=wiki_ner_data_file_name)
-    wiki_token_ner_data_file_name = join(args.output_path, 'token_' + args.data_name)
-    with open(wiki_token_ner_data_file_name, 'w') as f:
-        json.dump(token_data_examples, f)
+    print(type(dev_full_data))
+    test_full_data = []
+    test_col_names = ['_id', 'question', 'context']
+
+    for row in tqdm(dev_full_data):
+        test_row = dict([(key, value) for key, value in row.items() if key in test_col_names])
+        test_full_data.append(test_row)
+
+    json.dump(test_full_data, open(test_data_file_name, 'w'))
+
+    with open(test_data_file_name, 'r', encoding='utf-8') as reader:
+        test_full_data = json.load(reader)
+
+    for row in tqdm(test_full_data):
+        print(row)
