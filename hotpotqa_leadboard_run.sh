d2251d1dd7dc2ef1666bd0160900cf8d6d688058
guangtao.wang <guangtao.wang@jd.com>
2021-04-26 16:57:36 -0700

    save model

--- hotpotqa_leadboard_run.sh
+++ hotpotqa_leadboard_run.sh
@@ -48,14 +48,14 @@ preprocess() {
 #        # Output: ner.json
 #        python leaderboardscripts/2_lb_extract_ner.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json
 
-        echo "3. Paragraph ranking (1): longformer retrieval data preprocess"
-        # Output: para_ir_combined.json
-        python leaderboardscripts/3_lb_longformer_dataprepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
+#        echo "3. Paragraph ranking (1): longformer retrieval data preprocess"
+#        # Output: para_ir_combined.json
+#        python leaderboardscripts/3_lb_longformer_dataprepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
 
-#        echo "3. Paragraph ranking (2): longformer retrieval ranking scores"
-#        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
-#        # Output: long_para_ranking.json
-#        python leaderboardscripts/3_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
+        echo "3. Paragraph ranking (2): longformer retrieval ranking scores"
+        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
+        # Output: long_para_ranking.json
+        python leaderboardscripts/3_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
 
 #        echo "3. MultiHop Paragraph Selection (3)"
 #        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
