c1660260d888572e5df388811e8b78f77e99927c
guangtao.wang <guangtao.wang@jd.com>
2021-04-26 11:10:38 -0700

    save model

--- hotpotqa_leadboard_run.sh
+++ hotpotqa_leadboard_run.sh
@@ -41,7 +41,7 @@ preprocess() {
         echo "1. Extract Wiki Link & NER from DB"
         # Input: INPUT_FILE, enwiki_ner.db
         # Output: doc_link_ner.json
-        python leaderboardscripts/1_extract_db.py $INPUT_FILE $DATA_ROOT/knowledge/enwiki_ner.db $OUTPUT_PROCESSED/doc_link_ner.json
+        python leaderboardscripts/1_lb_extract_db.py $INPUT_FILE $DATA_ROOT/knowledge/enwiki_ner.db $OUTPUT_PROCESSED/doc_link_ner.json
 #
 #        echo "2. Extract NER for Question and Context"
 #        # Input: doc_link_ner.json
