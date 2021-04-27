1b22b1dbf970fd641e156db55448f1b2c21d7d9c
guangtao.wang <guangtao.wang@jd.com>
2021-04-26 19:18:44 -0700

    save model

--- hotpotqa_leadboard_run.sh
+++ hotpotqa_leadboard_run.sh
@@ -52,15 +52,15 @@ preprocess() {
 #        # Output: para_ir_combined.json
 #        python leaderboardscripts/3_lb_longformer_dataprepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
 
-        echo "3. Paragraph ranking (2): longformer retrieval ranking scores"
-        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
-        # Output: long_para_ranking.json
-        python leaderboardscripts/3_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
+#        echo "3. Paragraph ranking (2): longformer retrieval ranking scores"
+#        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
+#        # Output: long_para_ranking.json
+#        python leaderboardscripts/3_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
 
-#        echo "3. MultiHop Paragraph Selection (3)"
-#        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
-#        # Output: long_multihop_para.json
-#        python leaderboardscripts/3_lb_longformer_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json $SELECTEED_DOC_NUM
+        echo "3. MultiHop Paragraph Selection (3)"
+        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
+        # Output: long_multihop_para.json
+        python leaderboardscripts/3_lb_longformer_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json $SELECTEED_DOC_NUM
 
 #        echo "4. Dump features for electra (5) do_lower_case"
 #        python jdscripts/5_ext_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path $ELECTRA_ROOT/electra-large-discriminator --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type electra --tokenizer_name $ELECTRA_ROOT/electra-large-discriminator --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json --ranker long --data_type $DATA_TYPE
