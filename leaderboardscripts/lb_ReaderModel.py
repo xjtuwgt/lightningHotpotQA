67aed9e4fff82ab77d217f53be1d7b839e40d62c
guangtao.wang <guangtao.wang@jd.com>
2021-04-27 08:38:22 -0700

    save model

--- leaderboardscripts/lb_ReaderModel.py
+++ leaderboardscripts/lb_ReaderModel.py
@@ -10,18 +10,18 @@ class UnifiedHGNModel(nn.Module):
         self.config = config
         self.encoder, _ = load_encoder_model(self.config.encoder_name_or_path, self.config.model_type)
         self.model = HierarchicalGraphNetwork(config=self.config)
-        if self.config.encoder_path is not None:
-            logging.info("Loading encoder from: {}".format(self.config.encoder_path))
-            self.encoder.load_state_dict(torch.load(self.config.encoder_path))
-            logging.info("Loading encoder completed")
-        else:
-            raise 'The encoder name is none {}'.format(self.config.model)
-        if self.config.model_path is not None:
-            logging.info("Loading model from: {}".format(self.config.model_path))
-            self.encoder.load_state_dict(torch.load(self.config.model_path))
-            logging.info("Loading model completed")
-        else:
-            raise 'The model name is none'.format(self.config.model)
+        # if self.config.encoder_path is not None:
+        #     logging.info("Loading encoder from: {}".format(self.config.encoder_path))
+        #     self.encoder.load_state_dict(torch.load(self.config.encoder_path))
+        #     logging.info("Loading encoder completed")
+        # else:
+        #     raise 'The encoder name is none {}'.format(self.config.model)
+        # if self.config.model_path is not None:
+        #     logging.info("Loading model from: {}".format(self.config.model_path))
+        #     self.encoder.load_state_dict(torch.load(self.config.model_path))
+        #     logging.info("Loading model completed")
+        # else:
+        #     raise 'The model name is none'.format(self.config.model)
 
     def forward(self, batch, return_yp=True, return_cls=True):
         ###############################################################################################################
