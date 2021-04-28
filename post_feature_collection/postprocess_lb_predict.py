b4cd90f48de359fbae8ed057d4eb61fcf5bf635d
guangtao.wang <guangtao.wang@jd.com>
2021-04-28 15:59:57 -0700

    save model

---
+++ post_feature_collection/postprocess_lb_predict.py
@@ -0,0 +1,182 @@
+import logging
+import os
+import argparse
+from os.path import join
+import json
+# from leaderboardscripts.lb_hotpotqa_data_structure import DataHelper
+from plmodels.pldata_processing import DataHelper
+from envs import OUTPUT_FOLDER, DATASET_FOLDER
+import torch
+from utils.gpu_utils import single_free_cuda
+from leaderboardscripts.lb_ReaderModel import UnifiedHGNModel
+from leaderboardscripts.lb_hotpotqa_evaluation import jd_unified_test_model, jd_unified_eval_model
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def boolean_string(s):
+    if s.lower() not in {'false', 'true'}:
+        raise ValueError('Not a valid boolean string')
+    return s.lower() == 'true'
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='Evaluating albert based reader Model')
+    ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    parser.add_argument("--daug_type", default='long_low', type=str, help="Train Data augumentation type.")
+    parser.add_argument("--devf_type", default='long_low', type=str, help="Dev data type")
+
+    parser.add_argument('--output_dir',
+                        type=str,
+                        default=OUTPUT_FOLDER,
+                        help='Directory to save model and summaries')
+    parser.add_argument("--exp_name",
+                        type=str,
+                        default='albert_orig',
+                        help="If set, this will be used as directory name in OUTOUT folder")
+    parser.add_argument("--config_file",
+                        type=str,
+                        default=None,
+                        help="configuration file for command parser")
+    ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    # parser.add_argument('--input_data', default=None, type=str, required=True)
+    parser.add_argument("--encoder_ckpt", default='encoder.pkl', type=str)
+    parser.add_argument("--model_ckpt", default='model.pkl', type=str)
+    ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    # hyper-parameter
+    parser.add_argument('--q_update', type=boolean_string, default='False', help='Whether update query')
+    parser.add_argument("--trans_drop", type=float, default=0.2)
+    parser.add_argument("--trans_heads", type=int, default=3)
+    parser.add_argument("--do_rerank", action='store_true', help="Whether re-rank")
+
+
+    # graph
+    parser.add_argument('--num_edge_type', type=int, default=8)  ### number of edge types
+    parser.add_argument('--mask_edge_types', type=str, default="0")  ### masked edge types
+
+    parser.add_argument('--gnn', default='gat:1,2', type=str, help='gat:n_layer, n_head')
+    parser.add_argument("--gnn_drop", type=float, default=0.3)
+    #########
+    parser.add_argument("--gnn_attn_drop", type=float, default=0.3)
+    #########
+    parser.add_argument('--q_attn', type=boolean_string, default='True', help='whether use query attention in GAT')
+    parser.add_argument("--lstm_drop", type=float, default=0.3)
+    parser.add_argument("--lstm_layer", type=int, default=1)  ###++++++
+    parser.add_argument('--graph_residual', type=boolean_string, default='True',
+                        help='whether use residual connection in GAT')  ##+++++++++
+
+    parser.add_argument("--max_para_num", default=5, type=int)
+    parser.add_argument("--max_sent_num", default=40, type=int)
+    parser.add_argument("--max_entity_num", default=60, type=int)
+    parser.add_argument("--max_ans_ent_num", default=15, type=int)
+    parser.add_argument("--max_seq_length", default=512, type=int)
+    parser.add_argument("--max_query_length", default=50, type=int)
+
+    # bi attn
+    parser.add_argument('--ctx_attn', type=str, default='gate_att_up',
+                        choices=['no_gate', 'gate_att_or', 'gate_att_up'])
+    parser.add_argument("--ctx_attn_hidden_dim", type=int, default=300)
+    parser.add_argument("--bi_attn_drop", type=float, default=0.3)
+    parser.add_argument("--hidden_dim", type=int, default=300)
+    # ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    parser.add_argument('--topk_para_num', default=3, type=int, required=True)
+    ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    # parser.add_argument("--eval_ckpt", default=None, type=str, required=True, help="evaluation checkpoint")
+    parser.add_argument("--encoder_name_or_path",
+                        default='albert-xxlarge-v2',
+                        type=str,
+                        help="Path to pre-trained model or shortcut name selected")
+    parser.add_argument("--model_type", default='albert', type=str, help="alber reader model")
+    parser.add_argument('--gpus', default=1, type=int)
+    parser.add_argument('--test_batch_size', default=16, type=int)
+    parser.add_argument('--test_log_steps', default=10, type=int)
+    parser.add_argument('--cpu_num', default=24, type=int)
+    parser.add_argument("--dev_gold_file",
+                        type=str,
+                        default=join(DATASET_FOLDER, 'data_raw', 'hotpot_dev_distractor_v1.json'))
+
+    parser.add_argument("--para_path",
+                        type=str,
+                        default=join(DATASET_FOLDER, 'data_processed/test_distractor', 'rerank_topk_3_long_low_long_multihop_para.json'))
+    parser.add_argument("--data_type", type=str, required=True)
+    ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    return parser.parse_args(args)
+
+def complete_default_test_parser(args):
+    if torch.cuda.is_available():
+        device_ids, _ = single_free_cuda()
+        device = torch.device('cuda:{}'.format(device_ids[0]))
+    else:
+        device = torch.device('cpu')
+    args.device = device
+    args.num_gnn_layers = int(args.gnn.split(':')[1].split(',')[0])
+    args.num_gnn_heads = int(args.gnn.split(':')[1].split(',')[1])
+    if len(args.mask_edge_types):
+        args.mask_edge_types = list(map(int, args.mask_edge_types.split(',')))
+    # TODO: only support albert-xxlarge-v2 now
+    args.input_dim = 768 if 'base' in args.encoder_name_or_path else (4096 if 'albert' in args.encoder_name_or_path else 1024)
+    # output dir name
+    args.exp_name = os.path.join(args.output_dir, args.exp_name)
+    os.makedirs(args.exp_name, exist_ok=True)
+
+    encoder_path = join(args.exp_name, args.encoder_ckpt)  ## replace encoder.pkl as encoder
+    model_path = join(args.exp_name, args.model_ckpt)  ## replace encoder.pkl as encoder
+    args.encoder_path = encoder_path
+    args.model_path = model_path
+    return args
+
+#########################################################################
+# Initialize arguments
+##########################################################################
+args = parse_args()
+args = complete_default_test_parser(args=args)
+if args.do_rerank:
+    assert args.topk_para_num >= 2
+    args.testf_type = '{}_reranker{}'.format(args.testf_type, args.topk_para_num)
+
+
+logger.info('-' * 100)
+logger.info('Input Argument Information')
+logger.info('-' * 100)
+args_dict = vars(args)
+for a in args_dict:
+    logger.info('%-28s  %s' % (a, args_dict[a]))
+
+#########################################################################
+# Read Data
+##########################################################################
+helper = DataHelper(gz=True, config=args)
+# Set datasets
+data_example_dict = helper.test_example_dict
+data_feature_dict = helper.test_feature_dict
+data_features = helper.test_features
+data_data_loader = helper.hotpot_test_dataloader
+#
+# # # #########################################################################
+# # # # Initialize Model
+# # # ##########################################################################
+model = UnifiedHGNModel(config=args)
+model.to(args.device)
+
+output_pred_file = join(args.exp_name, 'test_pred.json')
+output_eval_file = join(args.exp_name, 'test_eval.txt')
+output_score_file = join(args.exp_name, 'dev_score.json')
+
+
+best_metrics, best_threshold = jd_unified_eval_model(args, model, test_data_loader, test_example_dict, test_feature_dict,
+                                output_pred_file, output_eval_file, args.dev_gold_file)
+for key, val in best_metrics.items():
+    print("{} = {}".format(key, val))
+print('Best threshold = {}'.format(best_threshold))
+
+threshold = best_threshold
+
+metrics = jd_unified_test_model(args, model,
+                                test_data_loader, test_example_dict, test_feature_dict,
+                                output_pred_file, output_eval_file, threshold, args.dev_gold_file)
+for key, val in metrics.items():
+    print("{} = {}".format(key, val))
---
+++ post_process_run.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+# DEFINE data related (please make changes according to your configurations)
+# DATA ROOT folder where you put data files
+DATA_ROOT=./data/
+
+LONG_FORMER_ROOT=allenai
+SELECTEED_DOC_NUM=6
+TOPK_PARA_NUM=4
+
+
+PROCS=${1:-"download"} # define the processes you want to run, e.g. "download,preprocess,train" or "preprocess" only
+
+# define precached BERT MODEL path
+# ROBERTA_LARGE=$DATA_ROOT/models/pretrained/roberta-large
+# pip install -U spacy
+# python -m spacy download en_core_web_lg
+
+# Add current pwd to PYTHONPATH
+export DIR_TMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH=$PYTHONPATH:$DIR_TMP:$DIR_TMP/hgntransformers
+export PYTORCH_PRETRAINED_BERT_CACHE=$DATA_ROOT/models/pretrained_cache
+
+mkdir -p $DATA_ROOT/models/pretrained_cache
+
+preprocess() {
+#    INPUTS=("hotpot_dev_distractor_v1.json;dev_distractor" "hotpot_train_v1.1.json;train")
+#    INPUTS=("hotpot_dev_distractor_v1.json;dev_distractor")
+    for input in ${INPUTS[*]}; do
+        INPUT_FILE=$(echo $input | cut -d ";" -f 1)
+        DATA_TYPE=$(echo $input | cut -d ";" -f 2)
+
+        echo "Processing input_file: ${INPUT_FILE}"
+
+        INPUT_FILE=$DATA_ROOT/dataset/data_raw/$INPUT_FILE
+        OUTPUT_PROCESSED=$DATA_ROOT/dataset/data_processed/$DATA_TYPE
+        OUTPUT_FEAT=$DATA_ROOT/dataset/data_feat/$DATA_TYPE
+
+        [[ -d $OUTPUT_PROCESSED ]] || mkdir -p $OUTPUT_PROCESSED
+        [[ -d $OUTPUT_FEAT ]] || mkdir -p $OUTPUT_FEAT
+
+        echo "1. Extract Wiki Link & NER from DB"
+        # Input: INPUT_FILE, enwiki_ner.db
+        # Output: doc_link_ner.json
+        python leaderboardscripts/1_lb_extract_db.py $INPUT_FILE $DATA_ROOT/knowledge/enwiki_ner.db $OUTPUT_PROCESSED/doc_link_ner.json
+
+        echo "2. Extract NER for Question and Context"
+        # Input: doc_link_ner.json
+        # Output: ner.json
+        python leaderboardscripts/2_lb_extract_ner.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json
+
+        echo "3. Paragraph ranking (1): longformer retrieval data preprocess"
+        # Output: para_ir_combined.json
+        python leaderboardscripts/3_lb_longformer_dataprepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
+
+        echo "3. Paragraph ranking (2): longformer retrieval ranking scores"
+        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
+        # Output: long_para_ranking.json
+        python leaderboardscripts/3_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
+
+        echo "3. MultiHop Paragraph Selection (3)"
+        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
+        # Output: long_multihop_para.json
+        python leaderboardscripts/3_lb_longformer_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json $SELECTEED_DOC_NUM
+
+#        echo "4. Dump features for albert do_lower_case"
+#        # Input: $INPUT_FILE, long_multihop_para.json; model_type, model_name, doc_link_ner.json, ner.json
+#        python post_feature_collection/5_lb_hotpotqa_dump_features.py  --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path albert-xxlarge-v2 --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type albert --tokenizer_name albert-xxlarge-v2 --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json --ranker long --data_type $DATA_TYPE --max_para_num $SELECTEED_DOC_NUM --topk $TOPK_PARA_NUM
+
+#        echo "5. Re-rank over top 5 via the trained model"
+#        python leaderboardscripts/5_lb_topk_rerankering.py --testf_type long_low --max_para_num $SELECTEED_DOC_NUM --topk_para_num $TOPK_PARA_NUM
+
+#        echo "6. Re-rank over top k and hyper-link "
+#        # output: topk_long_multihop_para.json
+#        python leaderboardscripts/5_lb_topk_hgnmodel_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED $SELECTEED_DOC_NUM $TOPK_PARA_NUM long_low
+#
+#        echo "7. Dump features for albert do_lower_case for re-rank results"
+#        python leaderboardscripts/4_lb_ext_dump_features.py --para_path $OUTPUT_PROCESSED --full_data $INPUT_FILE --model_name_or_path albert-xxlarge-v2 --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type albert --tokenizer_name albert-xxlarge-v2 --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json --ranker long --data_type $DATA_TYPE --do_rerank --topk $TOPK_PARA_NUM --max_para_num $SELECTEED_DOC_NUM
+
+#        echo "8. Model prediction with albert"
+#        python leaderboardscripts/lb_hotpotqa_predict.py --testf_type long_low --max_para_num $SELECTEED_DOC_NUM --topk_para_num $TOPK_PARA_NUM --do_rerank
+
+#        echo "7. Prediction postprocessing"
+    done
+
+}
+
+for proc in "download" "preprocess"
+do
+    if [[ ${PROCS:-"download"} =~ $proc ]]; then
+        $proc
+    fi
+done
