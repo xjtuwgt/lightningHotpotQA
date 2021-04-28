42d76445dbb16d0671f43eceeebbeb62eed19abf
guangtao.wang <guangtao.wang@jd.com>
2021-04-28 09:44:48 -0700

    save model

---
+++ leaderboardscripts/lb_postprocess_utils.py
@@ -0,0 +1,88 @@
+import numpy as np
+from numpy import ndarray
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def distribution_feat_extraction(scores: ndarray, keep_num=False):
+    ##     min, max, mean, median, 1/4, 3/4 score, std, num
+    ##gap: min, max, mean, median, 1/4, 3/4 score, std
+    min_value, max_value, mean_value, median_value, std_value = np.min(scores), np.max(scores), np.mean(scores), np.median(scores), np.std(scores)
+    quartile_1, quartile_2 = np.percentile(scores, 25), np.percentile(scores, 75)
+    quartile_3, quartile_4, quartile_5, quartile_6, quartile_7 = np.percentile(scores, 20), \
+                                                                 np.percentile(scores, 40), \
+                                                                 np.percentile(scores, 60), \
+                                                                 np.percentile(scores, 80), \
+                                                                 np.percentile(scores, 90)
+    dist_feat = [min_value, max_value, mean_value, median_value, std_value, quartile_1, quartile_2, quartile_3, quartile_4, quartile_5, quartile_6, quartile_7]
+    # dist_feat = [min_value, max_value, mean_value, median_value, std_value, quartile_1, quartile_2]
+    dist_feat = [x.tolist() for x in dist_feat]
+    if keep_num:
+        num = float(scores.shape[0])
+        dist_feat.append(num)
+    return dist_feat
+
+def distribution_feat(scores: ndarray):
+    dist_feat = distribution_feat_extraction(scores=scores, keep_num=True)
+    if scores.shape[0] > 1:
+        sorted_score = np.sort(scores)
+        reverse_score = sorted_score[::-1]
+        gap_scores = np.array([reverse_score[i] - reverse_score[i+1] for i in range(scores.shape[0]-1)])
+        gap_dist_feat = distribution_feat_extraction(scores=gap_scores, keep_num=False)
+    else:
+        gap_dist_feat = [0.0] * (len(dist_feat) - 1)
+    dist_feat.extend(gap_dist_feat)
+    return dist_feat
+
+def row_x_feat_extraction(row):
+    x_feats = row['cls_emb']
+    # ++++++++++++++++++++++++++++++
+    sent_scores = row['sp_score']
+    sent_mask = row['sp_mask']
+    sent_num = int(sum(sent_mask))
+    sent_score_np = np.array(sent_scores[:sent_num])
+    sent_feats = distribution_feat(scores=sent_score_np)
+    x_feats += sent_feats
+    return x_feats
+
+def load_npz_data(npz_file_name):
+    with np.load(npz_file_name) as data:
+        x = data['x']
+        y = data['y']
+        y_n = data['y_n']
+        y_np = data['y_np']
+    print('Loading {} records from {}'.format(x.shape[0], npz_file_name))
+    return x, y, y_n, y_np
+
+###+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+import torch
+from torch.utils.data import Dataset
+class RangeDataset(Dataset):
+    def __init__(self, npz_file_name):
+        x, y_p, y_n, _ = load_npz_data(npz_file_name)
+        self.x_feat = x
+        self.y_p = y_p
+        self.y_n = y_n
+
+    def __len__(self):
+        return self.x_feat.shape[0]
+
+    def __getitem__(self, idx):
+        x_i = torch.from_numpy(self.x_feat[idx]).float()
+        y_p_i, y_n_i = self.y_p[idx], self.y_n[idx]
+
+        if y_p_i > y_n_i:
+            y_min = torch.FloatTensor([y_n_i])
+            y_max = torch.FloatTensor([y_p_i])
+            flag = torch.LongTensor([1])
+        else:
+            y_min = torch.FloatTensor([y_p_i])
+            y_max = torch.FloatTensor([y_n_i])
+            flag = torch.LongTensor([0])
+        return x_i, y_min, y_max, flag
+
+    @staticmethod
+    def collate_fn(data):
+        x = torch.stack([_[0] for _ in data], dim=0)
+        y_min = torch.cat([_[1] for _ in data], dim=0)
+        y_max = torch.cat([_[2] for _ in data], dim=0)
+        flag = torch.cat([_[3] for _ in data], dim=0)
+        sample = {'x_feat': x, 'y_min': y_min, 'y_max': y_max, 'flag': flag}
+        return sample
---
+++ leaderboardscripts/lb_postprrocess_model.py
@@ -0,0 +1,151 @@
+import torch
+from torch import Tensor as T
+from torch import nn
+import torch.nn.functional as F
+
+
+class PositionwiseFeedForward(nn.Module):
+    "Implements FFN equation."
+    def __init__(self, model_dim, d_hidden, out_dim, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.output = nn.Sequential(
+            nn.Linear(model_dim, d_hidden * 4),
+            nn.ReLU(),
+            LayerNorm(d_hidden * 4, eps=1e-12),
+            nn.Dropout(dropout),
+            nn.Linear(d_hidden * 4, out_dim),
+        )
+
+    def forward(self, hidden_states):
+        return self.output(hidden_states)
+
+# class PositionwiseFeedForward(nn.Module):
+#     "Implements FFN equation."
+#     def __init__(self, model_dim, d_hidden, out_dim, dropout=0.1):
+#         super(PositionwiseFeedForward, self).__init__()
+#         self.w_1 = nn.Linear(model_dim, d_hidden)
+#         self.w_2 = nn.Linear(d_hidden, out_dim)
+#         self.dropout = nn.Dropout(dropout)
+#         self.init()
+#
+#     def forward(self, x):
+#         return self.w_2(self.dropout(F.relu(self.w_1(x))))
+#
+#     def init(self):
+#         gain = nn.init.calculate_gain('relu')
+#         nn.init.xavier_normal_(self.w_1.weight, gain=gain)
+#         nn.init.xavier_normal_(self.w_2.weight, gain=gain)
+
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        """Construct a layernorm module in the TF style (epsilon inside the square root).
+        """
+        super(LayerNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size), requires_grad=True)
+        self.bias = nn.Parameter(torch.zeros(hidden_size), requires_grad=True)
+        self.variance_epsilon = eps
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+        return self.weight * x + self.bias
+
+
+class OutputLayer(nn.Module):
+    def __init__(self, hidden_dim, trans_drop=0.25, num_answer=1):
+        super(OutputLayer, self).__init__()
+
+        self.output = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim*2),
+            nn.ReLU(),
+            LayerNorm(hidden_dim*2, eps=1e-12),
+            nn.Dropout(trans_drop),
+            #+++++++++
+            nn.Linear(2*hidden_dim, hidden_dim*2),
+            nn.ReLU(),
+            LayerNorm(hidden_dim * 2, eps=1e-12),
+            nn.Dropout(trans_drop),
+            nn.Linear(hidden_dim * 2, hidden_dim * 2),
+            nn.ReLU(),
+            LayerNorm(hidden_dim * 2, eps=1e-12),
+            nn.Dropout(trans_drop),
+            nn.Linear(hidden_dim * 2, hidden_dim * 2),
+
+            nn.ReLU(),
+            LayerNorm(hidden_dim * 2, eps=1e-12),
+            nn.Dropout(trans_drop),
+            nn.Linear(hidden_dim * 2, hidden_dim * 2),
+
+            # nn.Linear(2 * hidden_dim, hidden_dim * 2),
+            nn.ReLU(),
+            LayerNorm(hidden_dim * 2, eps=1e-12),
+            nn.Dropout(trans_drop),
+            nn.Linear(hidden_dim * 2, num_answer),
+            #+++++++++
+            # nn.Linear(hidden_dim*2, num_answer),
+        )
+
+    def forward(self, hidden_states):
+        return self.output(hidden_states)
+
+class RangeModel(nn.Module):
+    def __init__(self, args):
+        super(RangeModel, self).__init__()
+        self.args = args
+        self.cls_emb_dim = self.args.cls_emb_dim
+        self.emb_dim = self.args.emb_dim
+        self.score_dim = self.emb_dim - self.cls_emb_dim
+        self.hid_dim = self.args.hid_dim
+
+        self.cls_map = PositionwiseFeedForward(model_dim=self.cls_emb_dim,
+                                               d_hidden=1024, out_dim=self.hid_dim)
+        self.score_map = PositionwiseFeedForward(model_dim=self.score_dim,
+                                               d_hidden=1024, out_dim=self.hid_dim)
+        self.threshold_score_func = OutputLayer(hidden_dim=2 * self.hid_dim,
+                                           trans_drop=self.args.feat_drop,
+                                           num_answer=1)
+
+        # self.threshold_score_func = OutputLayer(hidden_dim=self.score_dim,
+        #                                         trans_drop=self.args.feat_drop,
+        #                                         num_answer=1)
+
+        # self.threshold_score_func = OutputLayer(hidden_dim=self.cls_emb_dim,
+        #                                         trans_drop=self.args.feat_drop,
+        #                                         num_answer=1)
+        # self.threshold_score_func = OutputLayer(hidden_dim=self.hid_dim,
+        #                                         trans_drop=self.args.feat_drop,
+        #                                         num_answer=1)
+
+        # self.threshold_score_func = OutputLayer(hidden_dim=self.emb_dim,
+        #                                         trans_drop=self.args.feat_drop,
+        #                                         num_answer=1)
+    def forward(self, x: T):
+        assert x.shape[1] == self.emb_dim
+        cls_x = x[:,:self.cls_emb_dim]
+        score_x = x[:,self.cls_emb_dim:]
+        cls_map_emb = self.cls_map.forward(cls_x)
+        score_map_emb = self.score_map.forward(score_x)
+        x_emb = torch.cat([cls_map_emb, score_map_emb], dim=-1)
+        # scores = self.threshold_score_func.forward(score_x)
+        # scores = self.threshold_score_func.forward(x)
+        # scores = self.threshold_score_func.forward(cls_x)
+        # scores = self.threshold_score_func.forward(score_map_emb)
+        # scores = self.threshold_score_func.forward(cls_map_emb)
+        scores = self.threshold_score_func.forward(x_emb)
+        return scores
+
+def loss_computation(scores, y_min, y_max):
+    # p_score = F.sigmoid(scores)
+    p_score = scores
+    # print(y_min)
+    # print(y_max)
+    # print(p_score)
+    loss = F.relu(p_score - y_max) + F.relu(y_min - p_score)
+    # loss = F.relu(torch.tanh(p_score) - torch.tanh(y_max)) + F.relu(torch.tanh(y_min) - torch.tanh(p_score))
+    loss = loss * loss
+    # loss = loss.mean()
+    loss = loss.sum()
+    return loss
+
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
