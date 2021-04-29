85cf14ef74c701031ba10569de87a975dd80e7c7
guangtao.wang <guangtao.wang@jd.com>
2021-04-28 17:24:13 -0700

    save model

--- post_feature_collection/5_lb_hotpotqa_dump_features.py
+++ post_feature_collection/5_lb_hotpotqa_dump_features.py
@@ -649,7 +649,7 @@ def build_graph(args, examples, features, entity_num):
     graphs = {}
     for case in tqdm(features):
         graph = create_graphs(case,
-                              max_para_num=4,
+                              max_para_num=args.max_para_num,
                               max_sent_num=args.max_sent_num,
                               max_entity_num=entity_num)
         graphs[case.qas_id] = {'adj': graph}
@@ -682,6 +682,7 @@ if __name__ == '__main__':
                         help="Set this flag if you are using an uncased model.")
     parser.add_argument("--max_entity_num", default=60, type=int)
     parser.add_argument("--max_sent_num", default=40, type=int)
+    parser.add_argument("--max_para_num", default=40, type=int)
     parser.add_argument("--max_query_length", default=50, type=int)
     parser.add_argument("--max_seq_length", default=512, type=int,
                         help="The maximum total input sequence length after WordPiece tokenization. Sequences longer "
