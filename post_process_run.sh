a9c0f30de4d20c487d55bc9cb0333777c7f756dd
guangtao.wang <guangtao.wang@jd.com>
2021-04-28 17:21:37 -0700

    save model

--- post_process_run.sh
+++ post_process_run.sh
@@ -25,8 +25,8 @@ mkdir -p $DATA_ROOT/models/pretrained_cache
 
 preprocess() {
 #    INPUTS=("hotpot_dev_distractor_v1.json;dev_distractor" "hotpot_train_v1.1.json;train")
-#    INPUTS=("hotpot_dev_distractor_v1.json;dev_distractor")
-    INPUTS=("hotpot_train_v1.1.json;train")
+    INPUTS=("hotpot_dev_distractor_v1.json;dev_distractor")
+#    INPUTS=("hotpot_train_v1.1.json;train")
     for input in ${INPUTS[*]}; do
         INPUT_FILE=$(echo $input | cut -d ";" -f 1)
         DATA_TYPE=$(echo $input | cut -d ";" -f 2)
@@ -40,24 +40,24 @@ preprocess() {
         [[ -d $OUTPUT_PROCESSED ]] || mkdir -p $OUTPUT_PROCESSED
         [[ -d $OUTPUT_FEAT ]] || mkdir -p $OUTPUT_FEAT
 
-        echo "1. Extract Wiki Link & NER from DB"
-        # Input: INPUT_FILE, enwiki_ner.db
-        # Output: doc_link_ner.json
-        python leaderboardscripts/1_lb_extract_db.py $INPUT_FILE $DATA_ROOT/knowledge/enwiki_ner.db $OUTPUT_PROCESSED/doc_link_ner.json
-
-        echo "2. Extract NER for Question and Context"
-        # Input: doc_link_ner.json
-        # Output: ner.json
-        python leaderboardscripts/2_lb_extract_ner.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json
-
-        echo "3. Paragraph ranking (1): longformer retrieval data preprocess"
-        # Output: para_ir_combined.json
-        python leaderboardscripts/3_lb_longformer_dataprepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
-
-        echo "3. Paragraph ranking (2): longformer retrieval ranking scores"
-        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
-        # Output: long_para_ranking.json
-        python leaderboardscripts/3_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
+#        echo "1. Extract Wiki Link & NER from DB"
+#        # Input: INPUT_FILE, enwiki_ner.db
+#        # Output: doc_link_ner.json
+#        python leaderboardscripts/1_lb_extract_db.py $INPUT_FILE $DATA_ROOT/knowledge/enwiki_ner.db $OUTPUT_PROCESSED/doc_link_ner.json
+#
+#        echo "2. Extract NER for Question and Context"
+#        # Input: doc_link_ner.json
+#        # Output: ner.json
+#        python leaderboardscripts/2_lb_extract_ner.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json
+#
+#        echo "3. Paragraph ranking (1): longformer retrieval data preprocess"
+#        # Output: para_ir_combined.json
+#        python leaderboardscripts/3_lb_longformer_dataprepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
+#
+#        echo "3. Paragraph ranking (2): longformer retrieval ranking scores"
+#        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
+#        # Output: long_para_ranking.json
+#        python leaderboardscripts/3_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
 
         echo "3. MultiHop Paragraph Selection (3)"
         # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
