f1d5a9d1df54369eb74f45255d4a0df52a042be9
guangtao.wang <guangtao.wang@jd.com>
2021-04-29 20:38:37 -0700

    save model

--- sent_replace_hotpotqa_run.sh
+++ sent_replace_hotpotqa_run.sh
@@ -19,8 +19,7 @@ export PYTORCH_PRETRAINED_BERT_CACHE=$DATA_ROOT/models/pretrained_cache
 mkdir -p $DATA_ROOT/models/pretrained_cache
 
 preprocess() {
-#    INPUTS=("hotpot_train_v1.1.json;train")
-    INPUTS=("hotpot_dev_distractor_v1.json;dev_distractor")
+    INPUTS=("hotpot_train_v1.1.json;train")
     for input in ${INPUTS[*]}; do
         INPUT_FILE=$(echo $input | cut -d ";" -f 1)
         DATA_TYPE=$(echo $input | cut -d ";" -f 2)
@@ -34,15 +33,15 @@ preprocess() {
         [[ -d $OUTPUT_PROCESSED ]] || mkdir -p $OUTPUT_PROCESSED
         [[ -d $OUTPUT_FEAT ]] || mkdir -p $OUTPUT_FEAT
 
-        echo "1. Splitting 10 paras into (4, 4, 2)"
-        # Input: INPUT_FILE, train_long_para_ranking.json
-        # Output: split_train_long_para_ranking.json
-        python sr_mhqa/hotpotqa_rank_split.py --full_data $INPUT_FILE --rank_data $OUTPUT_PROCESSED/train_long_para_ranking.json --split_rank_data $OUTPUT_PROCESSED/split_train_long_para_ranking.json
-
-        echo "2. Positive/negative para preprocess, tokenize (albert)"
-        # Input: INPUT_FILE, split_train_long_para_ranking.json
-        # Output: Example dictionary
-        python sr_mhqa/hotpotqa_sr_dump_examples.py --full_data $INPUT_FILE --split_rank_data $OUTPUT_PROCESSED/split_train_long_para_ranking.json --model_name_or_path albert-xxlarge-v2 --do_lower_case --model_type albert --tokenizer_name albert-xxlarge-v2 --output_dir $OUTPUT_FEAT  --ranker long --data_type $DATA_TYPE
+#        echo "1. Splitting 10 paras into (4, 4, 2)"
+#        # Input: INPUT_FILE, train_long_para_ranking.json
+#        # Output: split_train_long_para_ranking.json
+#        python sr_mhqa/hotpotqa_rank_split.py --full_data $INPUT_FILE --rank_data $OUTPUT_PROCESSED/train_long_para_ranking.json --split_rank_data $OUTPUT_PROCESSED/split_train_long_para_ranking.json
+#
+#        echo "2. Positive/negative para preprocess, tokenize (albert)"
+#        # Input: INPUT_FILE, split_train_long_para_ranking.json
+#        # Output: Example dictionary
+#        python sr_mhqa/hotpotqa_sr_dump_examples.py --full_data $INPUT_FILE --split_rank_data $OUTPUT_PROCESSED/split_train_long_para_ranking.json --model_name_or_path albert-xxlarge-v2 --do_lower_case --model_type albert --tokenizer_name albert-xxlarge-v2 --output_dir $OUTPUT_FEAT  --ranker long --data_type $DATA_TYPE
 
 #        echo "4. Drop sentence testing (albert low)"
 #        python sr_mhqa/hotpotqa_sr_test_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path albert-xxlarge-v2 --do_lower_case --model_type albert --tokenizer_name albert-xxlarge-v2 --output_dir $OUTPUT_FEAT  --ranker long --data_type $DATA_TYPE
