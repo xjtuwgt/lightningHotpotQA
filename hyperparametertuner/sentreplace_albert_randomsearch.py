f0d72324d4dc1b87e7824c347f0295d281e99360
guangtao.wang <guangtao.wang@jd.com>
2021-04-30 18:27:33 -0700

    save model

--- hyperparametertuner/sentreplace_albert_randomsearch.py
+++ hyperparametertuner/sentreplace_albert_randomsearch.py
@@ -19,7 +19,7 @@ def single_task_trial(search_space: dict, rand_seed=42):
         parameter_dict[key] = rand_search_parameter(value)
     parameter_dict['seed'] = rand_seed
     exp_name = 'train.graph.' + parameter_dict['model_type'] + '.bs' + str(parameter_dict['per_gpu_train_batch_size']) + '.as' + str(parameter_dict['gradient_accumulation_steps']) + \
-               '.lr' + str(parameter_dict['learning_rate']) + 'sdr.' + str(parameter_dict['sent_drop_ratio']) + \
+               '.lr' + str(parameter_dict['learning_rate']) + 'sdr.' + str(parameter_dict['sent_replace_ratio']) + \
                '.data' +str(parameter_dict['daug_type']) + parameter_dict['optimizer'] + '.' + parameter_dict['lr_scheduler'] + '.seed' +str(rand_seed)
     parameter_dict['exp_name'] = exp_name
     return parameter_dict
@@ -55,8 +55,8 @@ def HypeParameterSpace():
     daug_type = {'name': 'daug_type', 'type': 'choice', 'values': ['long_low']} #
     transformer_hidden_dim = {'name': 'transformer_hidden_dim', 'type': 'choice', 'values': [512]}
     transformer_head_num = {'name': 'transformer_head_num', 'type': 'choice', 'values': [8]}
-    sent_drop_ratio = {'name': 'sent_drop_ratio', 'type': 'choice', 'values': [0.1, 0.25]}
-    drop_prob = {'name': 'drop_prob', 'type': 'choice', 'values': [0.15, 0.25]}
+    sent_replace_ratio = {'name': 'sent_replace_ratio', 'type': 'choice', 'values': [0.1, 0.25]}
+    replace_prob = {'name': 'replace_prob', 'type': 'choice', 'values': [0.15, 0.25]}
     per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2]}
     model_type = {'name': 'model_type', 'type': 'choice', 'values': ['albert']}
     fine_tuned_encoder = {'name': 'fine_tuned_encoder', 'type': 'choice', 'values': ['albert/albert-xxlarge-v2_hotpotqa']} #'ahotrod/roberta_large_squad2'
@@ -65,8 +65,8 @@ def HypeParameterSpace():
     lr_scheduler = {'name': 'lr_scheduler', 'type': 'choice', 'values': ['cosine']}
     #++++++++++++++++++++++++++++++++++
     search_space = [learning_rate, per_gpu_train_batch_size, gradient_accumulation_steps, sent_lambda, layer_wise_lr_decay,
-                    lr_scheduler, fine_tuned_encoder, daug_type, devf_type,  optimizer, trans_drop, num_train_epochs, drop_prob,
-                    model_type, encoder_name_or_path, transformer_hidden_dim, transformer_head_num, sent_drop_ratio]
+                    lr_scheduler, fine_tuned_encoder, daug_type, devf_type,  optimizer, trans_drop, num_train_epochs, replace_prob,
+                    model_type, encoder_name_or_path, transformer_hidden_dim, transformer_head_num, sent_replace_ratio]
     search_space = dict((x['name'], x) for x in search_space)
     return search_space
 
@@ -89,7 +89,7 @@ def generate_random_search_bash(task_num, seed=42):
     search_space = HypeParameterSpace()
     for i in range(task_num):
         rand_hype_dict = single_task_trial(search_space, seed+i)
-        config_json_file_name = 'train.sent_drop.' + rand_hype_dict['model_type'] + '.data.' + rand_hype_dict['daug_type'] + 'sdr.' + str(rand_hype_dict['sent_drop_ratio']) \
+        config_json_file_name = 'train.sent_replace.' + rand_hype_dict['model_type'] + '.data.' + rand_hype_dict['daug_type'] + 'sdr.' + str(rand_hype_dict['sent_replace_ratio']) \
                                 +'.lr.'+ str(rand_hype_dict['learning_rate']) + rand_hype_dict['optimizer'] + '.' + rand_hype_dict['lr_scheduler']+ \
                                 '.seed' + str(rand_hype_dict['seed']) + '.json'
         with open(os.path.join(bash_save_path, config_json_file_name), 'w') as fp:
@@ -98,7 +98,7 @@ def generate_random_search_bash(task_num, seed=42):
         with open(jobs_path + 'jd_hotpotqa_' + config_json_file_name +'.sh', 'w') as rsh_i:
             # command_i = "CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 jdgraphtrain.py --config_file " + \
             #             json_file_path + config_json_file_name
-            command_i = "CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 jdsdhotpotqatrain.py --config_file " + \
+            command_i = "CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 jdsrhotpotqatrain.py --config_file " + \
                         json_file_path + config_json_file_name
             rsh_i.write(command_i)
             print('saving jobs at {}'.format(jobs_path + 'jd_hotpotqa_' + config_json_file_name +'.sh'))
