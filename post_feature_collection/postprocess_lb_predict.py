206007a8f4431daa7e3bb3ddedf16c30c2b3dbfe
guangtao.wang <guangtao.wang@jd.com>
2021-05-03 12:57:40 -0700

    save model

--- post_feature_collection/postprocess_lb_predict.py
+++ post_feature_collection/postprocess_lb_predict.py
@@ -94,6 +94,7 @@ def parse_args(args=None):
     parser.add_argument('--gpus', default=1, type=int)
     parser.add_argument('--test_batch_size', default=16, type=int)
     parser.add_argument('--eval_batch_size', default=16, type=int)
+    parser.add_argument('--batch_size', default=16, type=int)
     parser.add_argument('--test_log_steps', default=10, type=int)
     parser.add_argument('--cpu_num', default=24, type=int)
     parser.add_argument("--dev_gold_file",
