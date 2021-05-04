4e15960475742f2cfb65d4cba4d86235fceab6d2
guangtao.wang <guangtao.wang@jd.com>
2021-05-04 09:42:18 -0700

    save model

--- post_feature_collection/post_process_feature_extractor.py
+++ post_feature_collection/post_process_feature_extractor.py
@@ -1,6 +1,7 @@
 from post_feature_collection.post_process_argument_parser import train_parser
 from os.path import join
 from leaderboardscripts.lb_postprocess_utils import load_json_score_data
+from tqdm import tqdm
 
 def feat_label_extraction(raw_data_name, score_data_name):
     raw_data = load_json_score_data(raw_data_name)
@@ -8,11 +9,17 @@ def feat_label_extraction(raw_data_name, score_data_name):
     score_data = load_json_score_data(score_data_name)
     print('Loading {} records from {}'.format(len(score_data), score_data_name))
 
+    for case in tqdm(raw_data):
+        key = case['_id']
+        score_case = score_data[key]
+        print(score_case)
+
 def train_feature_label_extraction(args):
     raw_train_file_name = join(args.input_dir, args.raw_train_data)
     train_score_file_name = join(args.output_dir, args.exp_name, args.train_score_name)
     train_feat_file_name = join(args.output_dir, args.exp_name, args.train_feat_json_name)
     feat_label_extraction(raw_data_name=raw_train_file_name, score_data_name=train_score_file_name)
+
     return
 
 def dev_feature_label_extraction(args):
@@ -27,5 +34,5 @@ if __name__ == '__main__':
     for key, value in vars(args).items():
         print('{}: {}'.format(key, value))
     print('*' * 75)
-    train_feature_label_extraction(args=args)
+    # train_feature_label_extraction(args=args)
     dev_feature_label_extraction(args=args)
