73e52a66d66eaa1cd749290dea38f274046f4139
guangtao.wang <guangtao.wang@jd.com>
2021-05-04 09:30:23 -0700

    save model

--- post_feature_collection/post_process_feature_extractor.py
+++ post_feature_collection/post_process_feature_extractor.py
@@ -8,11 +8,14 @@ if __name__ == '__main__':
     args = train_parser()
     for key, value in vars(args).items():
         print('{}: {}'.format(key, value))
+    print('*' * 75)
 
     train_score_file_name = join(args.output_dir, args.exp_name, args.train_score_name)
     dev_score_file_name = join(args.output_dir, args.exp_name, args.dev_score_name)
-
+    start_time = time()
     train_score_data = load_json_score_data(train_score_file_name)
-    print('Loading {} records from {}'.format(len(train_score_data), train_score_file_name))
+    print('Loading {} records from {} in {:.4f} seconds'.format(len(train_score_data), train_score_file_name, time() - start_time))
+    start_time = time()
     dev_score_data = load_json_score_data(dev_score_file_name)
-    print('Loading {} records from {}'.format(len(dev_score_data), dev_score_file_name))
+    print('Loading {} records from {} in {:.4f} seconds '.format(len(dev_score_data), dev_score_file_name, time() - start_time))
+
