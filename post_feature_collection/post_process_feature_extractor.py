912e3a986a649e562b3de7cbf160b4d5790d8bf2
guangtao.wang <guangtao.wang@jd.com>
2021-05-04 09:15:35 -0700

    save model

--- post_feature_collection/post_process_feature_extractor.py
+++ post_feature_collection/post_process_feature_extractor.py
@@ -1,8 +1,7 @@
-import argparse
-from model_envs import MODEL_CLASSES
+from post_feature_collection.post_process_argument_parser import train_parser
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    # Required parameters
-    parser.add_argument("--score_file_path", type=str, required=True)
-    # Other parameters
+    args = train_parser()
+
+    for key, value in vars(args).items():
+        print('{}: {}'.format(key, value))
