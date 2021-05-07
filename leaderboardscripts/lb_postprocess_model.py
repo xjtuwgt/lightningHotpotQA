cc569b96e62516e246b7175b65320e97a39878a5
guangtao.wang <guangtao.wang@jd.com>
2021-05-06 22:45:54 -0700

    save model

--- leaderboardscripts/lb_postprocess_model.py
+++ leaderboardscripts/lb_postprocess_model.py
@@ -45,16 +45,16 @@ class OutputLayer(nn.Module):
             nn.ReLU(),
             LayerNorm(hidden_dim*2, eps=1e-12),
             nn.Dropout(trans_drop),
-            # nn.Linear(2*hidden_dim, hidden_dim*2),
+            nn.Linear(2*hidden_dim, hidden_dim*2),
 
-            # nn.ReLU(),
-            # LayerNorm(hidden_dim * 2, eps=1e-12),
-            # nn.Dropout(trans_drop),
-            # nn.Linear(hidden_dim * 2, hidden_dim * 2),
-            #
-            # nn.ReLU(),
-            # LayerNorm(hidden_dim * 2, eps=1e-12),
-            # nn.Dropout(trans_drop),
+            nn.ReLU(),
+            LayerNorm(hidden_dim * 2, eps=1e-12),
+            nn.Dropout(trans_drop),
+            nn.Linear(hidden_dim * 2, hidden_dim * 2),
+
+            nn.ReLU(),
+            LayerNorm(hidden_dim * 2, eps=1e-12),
+            nn.Dropout(trans_drop),
             # nn.Linear(hidden_dim * 2, hidden_dim * 2),
 
             # nn.ReLU(),
