0b827c1297be7eccbf8942ae5c297bfbc079b862
guangtao.wang <guangtao.wang@jd.com>
2021-05-07 11:37:22 -0700

    save model

--- post_feature_collection/post_process_argument_parser.py
+++ post_feature_collection/post_process_argument_parser.py
@@ -72,7 +72,7 @@ def train_parser():
                         help="Epsilon for Adam optimizer.")
     parser.add_argument("--max_grad_norm", default=1.0, type=float,
                         help="Max gradient norm.")
-    parser.add_argument("--num_train_epochs", default=300, type=int,
+    parser.add_argument("--num_train_epochs", default=800, type=int,
                         help="epochs")
     parser.add_argument("--warmup_steps", default=0, type=int,
                         help="epochs")
