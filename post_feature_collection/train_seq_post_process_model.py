62ccc2f041a1970be7e2e20fd90ec38e9c24fa3c
guangtao.wang <guangtao.wang@jd.com>
2021-05-07 09:21:30 -0700

    save model

--- post_feature_collection/train_seq_post_process_model.py
+++ post_feature_collection/train_seq_post_process_model.py
@@ -135,7 +135,7 @@ def eval_model(model, data_loader, dev_score_dict, threshold_category, alpha, de
             y_flag_np = batch['flag'].data.cpu().numpy()
             start_indexes = y1.data.cpu().numpy()
             end_indexes = y2.data.cpu().numpy()
-            y_1 = batch['y_1'].data.cpu().numpy()
+            gold_y_1 = batch['y_1'].data.cpu().numpy()
 
             for i in range(y_min_np.shape[0]):
                 key = batch['id'][i]
@@ -147,7 +147,9 @@ def eval_model(model, data_loader, dev_score_dict, threshold_category, alpha, de
                 y_max_i = np_sigmoid(y_max_np[i])
                 y_flag_i = y_flag_np[i]
 
-                score_i = threshold_category[y1[i] + 1][0]
+                if gold_y_1[i] > 0:
+                    score_i = threshold_category[gold_y_1[i]][0]
+
                 # print('pred', start_i, end_i)
                 # print('gold', batch['y_1'][i], batch['y_2'][i])
                 if key in dev_score_dict:
