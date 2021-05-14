d7b2bef55f0c37445e2c8f23caed09688d85ccb9
guangtao.wang <guangtao.wang@jd.com>
2021-05-13 17:12:34 -0700

    save model

--- post_feature_collection/post_process_argument_parser.py
+++ post_feature_collection/post_process_argument_parser.py
@@ -69,7 +69,7 @@ def train_parser():
     parser.add_argument("--hid_dim", type=int, default=512, help='cls_emb_dim')
     parser.add_argument("--cpu_number", type=int, default=6, help='cpu number')
     parser.add_argument("--interval_number", type=int, default=300, help='interval number')
-    parser.add_argument("--alpha", type=float, default=0.25, help='prediction alpha')
+    parser.add_argument("--alpha", type=float, default=0.1, help='prediction alpha')
     parser.add_argument("--weighted_loss", type=bool, default=False, help='weighted loss')
 
     parser.add_argument("--learning_rate", default=0.001, type=float,
