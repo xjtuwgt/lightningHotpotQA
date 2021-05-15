c4595abe89b7965a41c027079b97cee4c3e54288
guangtao.wang <guangtao.wang@jd.com>
2021-05-15 10:22:04 -0700

    save model

--- post_feature_collection/train_seq_post_process_model.py
+++ post_feature_collection/train_seq_post_process_model.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
     alpha_array = [0.1, 0.15, 0.2, 0.25]
     learning_rate_array = [0.0005, 0.001, 0.002]
     decoder_span_window_size_pair = [(180, 190), (170, 180)]
-    encoder_drop_out = [0.25, 0.3, 0.35, 0.4]
+    encoder_drop_out = [0.25, 0.3, 0.35]
 
     encoder_array = ['conv', 'ff']
     expriment_num = 0
