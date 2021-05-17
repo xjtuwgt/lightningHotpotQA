242b2089b9427d63a2d4841ec1518363bf0e8b05
guangtao.wang <guangtao.wang@jd.com>
2021-05-17 16:49:11 -0700

    save model

--- post_feature_collection/train_seq_post_process_model.py
+++ post_feature_collection/train_seq_post_process_model.py
@@ -191,8 +191,8 @@ def eval_model(model, data_loader, dev_score_dict, threshold_category, alpha, we
 if __name__ == '__main__':
 
     learning_rate_array = [0.001, 0.001, 0.003]
-    decoder_span_window_size_pair = [(185, 190), (170, 180)]
-    encoder_drop_out = [0.3]
+    decoder_span_window_size_pair = [(188, 190), (170, 180)]
+    encoder_drop_out = [0.25]
     trim_drop_ratio = [0.05, 0.1]
 
     encoder_array = ['conv', 'ff']
