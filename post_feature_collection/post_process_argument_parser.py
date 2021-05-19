d735910274c79f7637901519e6870f395211a038
guangtao.wang <guangtao.wang@jd.com>
2021-05-18 18:52:54 -0700

    save model

--- post_feature_collection/post_process_argument_parser.py
+++ post_feature_collection/post_process_argument_parser.py
@@ -53,11 +53,11 @@ def train_parser():
     parser.add_argument("--rand_seed", type=int, default=1234)
 
     parser.add_argument("--train_batch_size", type=int, default=4096, help='training batch size')
-    parser.add_argument("--eval_batch_size", type=int, default=1024, help='evaluation batch size')
+    parser.add_argument("--eval_batch_size", type=int, default=2048, help='evaluation batch size')
     parser.add_argument("--span_window_size", type=int, default=165, help='span_window_size')
     parser.add_argument("--decoder_window_size", type=int, default=180, help='span_window_size')
     parser.add_argument("--encoder_type", type=str, default='conv', help='the encoder type to fuse cls, and score: ff, conv, transformer')
-    parser.add_argument("--encoder_layer", type=int, default=2,
+    parser.add_argument("--encoder_layer", type=int, default=3,
                         help='number of layer in encoder')
     parser.add_argument("--encoder_hid_dim", type=int, default=512,
                         help='hid_dim of encoder')
